5b9de5ddbda1e61fd405c8837e684048e81a1ffb
Mor <mordbz@gmail.com>
2020-04-18 23:17:10 +0300

    initial disasm of FUN_020C9C0C

--- asm/rom2.s
+++ asm/rom2.s
@@ -2,7 +2,8 @@
 
 .section .text
 
-.incbin "baserom.nds", 0xD9914, 0x16CB8
+.incbin "baserom.nds", 0xD9150, 0x1747C
+
 
 	.global FUN_020EC5CC
 FUN_020EC5CC:
--- asm/secure.s
+++ asm/secure.s
@@ -2,4 +2,12 @@
 
 .section .text
 
-.incbin "baserom.nds", 0x4000, 0x800
+.incbin "baserom.nds", 0x4000, 0x5F2
+
+	.global FUN_020005F2
+	.thumb
+FUN_020005F2:
+	swi 3
+	bx lr
+
+.incbin "baserom.nds", 0x45F6, 0x20A
