97448e3eff5a63add4cc3a465b7ebcdba81d7112
PikalaxALT <PikalaxALT@gmail.com>
2020-04-19 11:03:43 -0400

    Dump file allocation table

--- asm/rom3.s
+++ asm/rom3.s
@@ -1,5 +1,262 @@
-/* rom3.s TODO: Disassemble */
-
 	.section .text
+	.incbin "baserom.nds", 0x10C400, 0x20
+	.balign 512, 255
 
-	.incbin "baserom.nds", 0x10C400, 0x200A20
+	.incbin "baserom.nds", 0x10C600, 0x20
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x10C800, 0x20
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x10CA00, 0x20
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x10CC00, 0x386E0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x145400, 0x261A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x16B600, 0x18D80
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x184400, 0x32E0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x187800, 0x1B760
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x1A3000, 0xA1A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x1AD200, 0x20
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x1AD400, 0x329E0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x1DFE00, 0x19260
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x1F9200, 0x2EBE0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x227E00, 0xF520
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x237400, 0x4680
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x23BC00, 0x28500
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x264200, 0x6E80
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x26B200, 0x17CE0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x283000, 0x100
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x283200, 0x2400
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x285600, 0x5C0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x285C00, 0xF00
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x286C00, 0x1900
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x288600, 0x9A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x289000, 0x560
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x289600, 0x800
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x289E00, 0xBA0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x28AA00, 0x1780
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x28C200, 0x900
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x28CC00, 0x600
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x28D200, 0x600
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x28D800, 0x4A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x28DE00, 0x320
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x28E200, 0x4C0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x28E800, 0x9A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x28F200, 0xCC0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x290000, 0x560
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x290600, 0xB40
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x291200, 0xCC0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x292000, 0xC60
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x292E00, 0x1140
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x294000, 0x900
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x294A00, 0x9C0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x295400, 0x4A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x295A00, 0x940
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x296400, 0x320
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x296800, 0xCA0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x297600, 0x8A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x298000, 0x600
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x298600, 0x320
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x298A00, 0xFC0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x299A00, 0x400
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x299E00, 0x2480
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x29C400, 0x2380
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x29E800, 0x3040
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2A1A00, 0x47A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2A6200, 0x2540
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2A8800, 0x15A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2A9E00, 0x2C20
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2ACC00, 0xD00
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2ADA00, 0x1080
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2AEC00, 0x3DE0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2B2A00, 0x4AC0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2B7600, 0x1C40
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2B9400, 0x3000
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2BC400, 0x2660
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2BEC00, 0x2440
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2C1200, 0x1600
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2C2800, 0x23A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2C4C00, 0xE20
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2C5C00, 0x3EC0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2C9C00, 0x20
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2C9E00, 0x4AE0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2CEA00, 0x1220
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2CFE00, 0x5DE0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2D5C00, 0x1E20
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2D7C00, 0x1BE0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2D9800, 0x460
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2D9E00, 0x52C0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2DF200, 0xA720
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2E9A00, 0x5580
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2EF000, 0x2540
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2F1600, 0xE2A0
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x2FFA00, 0x3920
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x303400, 0x9100
+	.balign 512, 255
+
+	.incbin "baserom.nds", 0x30C600, 0x820
+	.balign 512, 255
+
