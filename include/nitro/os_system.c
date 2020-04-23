50fa41f8771a0868cdc4d894802e8620e3bbe722
red031000 <rubenru09@aol.com>
2020-04-23 21:08:09 +0100

    match os_system.c

--- include/nitro/os_system.c
+++ include/nitro/os_system.c
@@ -4,31 +4,25 @@
 
 #include "os_system.h"
 
-OSIntrMode OS_DisableInterrupts() {
-    __asm {
-        mrs     r0, cpsr
-        orr     r1, r0, #HW_PSR_IRQ_DISABLE
-        msr     cpsr_c, r1
-        and     r0, r0, #HW_PSR_IRQ_DISABLE
-        bx      lr
-    }
+asm OSIntrMode OS_DisableInterrupts() {
+    mrs     r0, cpsr
+    orr     r1, r0, #HW_PSR_IRQ_DISABLE
+    msr     cpsr_c, r1
+    and     r0, r0, #HW_PSR_IRQ_DISABLE
+    bx      lr
 }
 
-OSIntrMode OS_RestoreInterrupts(OSIntrMode state) {
-    __asm {
-        mrs     r1, cpsr
-        bic     r2, r1, #HW_PSR_IRQ_DISABLE
-        orr     r2, r2, r0
-        msr     cpsr_c, r2
-        and     r0, r1, #HW_PSR_IRQ_DISABLE
-        bx      lr
-    }
+asm OSIntrMode OS_RestoreInterrupts(OSIntrMode state) {
+    mrs     r1, cpsr
+    bic     r2, r1, #HW_PSR_IRQ_DISABLE
+    orr     r2, r2, r0
+    msr     cpsr_c, r2
+    and     r0, r1, #HW_PSR_IRQ_DISABLE
+    bx      lr
 }
 
-OSProcMode OS_GetProcMode() {
-    __asm {
+asm OSProcMode OS_GetProcMode() {
     mrs r0, cpsr
     and r0, r0, #HW_PSR_CPU_MODE_MASK
     bx lr
-    }
 }
