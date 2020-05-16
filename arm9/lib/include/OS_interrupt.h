157c523cb52b6564612298ebe10c260fa0cbffc4
red031000 <rubenru09@aol.com>
2020-05-16 20:15:41 +0100

    match OS_interrupt (finally)

--- arm9/lib/include/OS_interrupt.h
+++ arm9/lib/include/OS_interrupt.h
@@ -26,5 +26,10 @@ void OS_SetIrqFunction(OSIrqMask intrBit, OSIrqFunction function);
 OSIrqFunction OS_GetIrqFunction(OSIrqMask intrBit);
 void OSi_EnterDmaCallback(u32 dmaNo, void (*callback) (void *), void *arg);
 void OSi_EnterTimerCallback(u32 timerNo, void (*callback) (void *), void *arg);
+OSIrqMask OS_SetIrqMask(OSIrqMask mask);
+OSIrqMask OS_EnableIrqMask(OSIrqMask mask);
+OSIrqMask OS_DisableIrqMask(OSIrqMask mask);
+OSIrqMask OS_ResetRequestIrqMask(OSIrqMask mask);
+void OS_SetIrqStackChecker(void);
 
 #endif //POKEDIAMOND_OS_INTERRUPT_H
