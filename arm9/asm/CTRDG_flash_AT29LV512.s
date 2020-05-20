d648a684ef6e337f22df8f7197c2dd6cd286743e
red031000 <rubenru09@aol.com>
2020-05-20 16:33:51 +0100

    split libctrdg

---
+++ arm9/asm/CTRDG_flash_AT29LV512.s
@@ -0,0 +1,503 @@
+	.include "asm/macros.inc"
+	.include "global.inc"
+
+    .text
+
+	arm_func_start CTRDGi_WriteFlashSectorAsyncAT
+CTRDGi_WriteFlashSectorAsyncAT: ; 0x020DC5DC
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	str r1, [sp, #0xc]
+	strh r0, [sp, #0x20]
+	ldr r1, _020DC604 ; =CTRDGi_WriteFlashSectorCoreAT
+	add r0, sp, #0x0
+	bl CTRDGi_SetTask
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+	.balign 4
+_020DC604: .word CTRDGi_WriteFlashSectorCoreAT
+
+	arm_func_start CTRDGi_EraseFlashSectorAsyncAT
+CTRDGi_EraseFlashSectorAsyncAT: ; 0x020DC608
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	mov r2, r1
+	strh r0, [sp, #0x20]
+	ldr r1, _020DC630 ; =CTRDGi_EraseFlashSectorCoreAT
+	add r0, sp, #0x0
+	bl CTRDGi_SetTask
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+	.balign 4
+_020DC630: .word CTRDGi_EraseFlashSectorCoreAT
+
+	arm_func_start CTRDGi_WriteFlashSectorAT
+CTRDGi_WriteFlashSectorAT: ; 0x020DC634
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	strh r0, [sp, #0x20]
+	add r0, sp, #0x0
+	str r1, [sp, #0xc]
+	bl CTRDGi_WriteFlashSectorCoreAT
+	mov r0, r0, lsl #0x10
+	mov r0, r0, lsr #0x10
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+
+	arm_func_start CTRDGi_EraseFlashSectorAT
+CTRDGi_EraseFlashSectorAT:
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	strh r0, [sp, #0x20]
+	add r0, sp, #0x0
+	bl CTRDGi_EraseFlashSectorCoreAT
+	mov r0, r0, lsl #0x10
+	mov r0, r0, lsr #0x10
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+
+	arm_func_start CTRDGi_WriteFlash4KBAsyncAT
+CTRDGi_WriteFlash4KBAsyncAT: ; 0x020DC688
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	str r1, [sp, #0xc]
+	strh r0, [sp, #0x20]
+	ldr r1, _020DC6B0 ; =CTRDGi_WriteFlash4KBCoreAT
+	add r0, sp, #0x0
+	bl CTRDGi_SetTask
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+	.balign 4
+_020DC6B0: .word CTRDGi_WriteFlash4KBCoreAT
+
+	arm_func_start CTRDGi_EraseFlash4KBAsyncAT
+CTRDGi_EraseFlash4KBAsyncAT: ; 0x020DC6B4
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	mov r2, r1
+	strh r0, [sp, #0x20]
+	ldr r1, _020DC6DC ; =CTRDGi_EraseFlash4KBCoreAT
+	add r0, sp, #0x0
+	bl CTRDGi_SetTask
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+	.balign 4
+_020DC6DC: .word CTRDGi_EraseFlash4KBCoreAT
+
+	arm_func_start CTRDGi_EraseFlashChipAsyncAT
+CTRDGi_EraseFlashChipAsyncAT: ; 0x020DC6E0
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	ldr r1, _020DC704 ; =CTRDGi_EraseFlashChipCoreAT
+	mov r2, r0
+	add r0, sp, #0x0
+	bl CTRDGi_SetTask
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+	.balign 4
+_020DC704: .word CTRDGi_EraseFlashChipCoreAT
+
+	arm_func_start CTRDGi_WriteFlash4KBAT
+CTRDGi_WriteFlash4KBAT: ; 0x020DC708
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	strh r0, [sp, #0x20]
+	add r0, sp, #0x0
+	str r1, [sp, #0xc]
+	bl CTRDGi_WriteFlash4KBCoreAT
+	mov r0, r0, lsl #0x10
+	mov r0, r0, lsr #0x10
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+
+	arm_func_start CTRDGi_EraseFlash4KBAT
+CTRDGi_EraseFlash4KBAT: ; 0x020DC734
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	strh r0, [sp, #0x20]
+	add r0, sp, #0x0
+	bl CTRDGi_EraseFlash4KBCoreAT
+	mov r0, r0, lsl #0x10
+	mov r0, r0, lsr #0x10
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+
+	arm_func_start CTRDGi_EraseFlashChipAT
+CTRDGi_EraseFlashChipAT: ; 0x020DC75C
+	stmdb sp!, {lr}
+	sub sp, sp, #0x24
+	add r0, sp, #0x0
+	bl CTRDGi_EraseFlashChipCoreAT
+	mov r0, r0, lsl #0x10
+	mov r0, r0, lsr #0x10
+	add sp, sp, #0x24
+	ldmia sp!, {lr}
+	bx lr
+
+	arm_func_start CTRDGi_WriteFlash4KBCoreAT
+CTRDGi_WriteFlash4KBCoreAT: ; 0x020DC780
+	stmdb sp!, {r4-r9,lr}
+	sub sp, sp, #0x24
+	mov r5, r0
+	ldmia r5!, {r0-r3}
+	add r4, sp, #0x0
+	stmia r4!, {r0-r3}
+	ldmia r5!, {r0-r3}
+	stmia r4!, {r0-r3}
+	ldr r0, [r5, #0x0]
+	str r0, [r4, #0x0]
+	ldrh r0, [sp, #0x20]
+	ldr r7, [sp, #0xc]
+	cmp r0, #0x10
+	addcs sp, sp, #0x24
+	ldrhs r0, _020DC85C ; =0x000080FF
+	ldmcsia sp!, {r4-r9,lr}
+	bxcs lr
+	ldr r1, _020DC860 ; =0x0210400C
+	mov r0, r0, lsl #0x15
+	ldr r1, [r1, #0x24]
+	ldr r4, _020DC864 ; =0x021D6B08
+	mov r8, r0, lsr #0x10
+	strh r1, [r4, #0x0]
+	ldrh r0, [r4, #0x0]
+	cmp r0, #0x0
+	beq _020DC84C
+	ldr r0, _020DC868 ; =0x02104048
+	ldr r6, [r0, #0x24]
+	mov r5, #0x2
+_020DC7F4:
+	mov r9, r5
+_020DC7F8:
+	mov r0, r8
+	mov r1, r7
+	bl CTRDGi_WriteFlashSectorAT
+	movs r12, r0
+	beq _020DC81C
+	sub r0, r9, #0x1
+	mov r0, r0, lsl #0x10
+	movs r9, r0, lsr #0x10
+	bne _020DC7F8
+_020DC81C:
+	cmp r12, #0x0
+	bne _020DC84C
+	ldrh r1, [r4, #0x0]
+	add r0, r8, #0x1
+	mov r0, r0, lsl #0x10
+	sub r1, r1, r6
+	strh r1, [r4, #0x0]
+	ldrh r1, [r4, #0x0]
+	add r7, r7, r6
+	mov r8, r0, lsr #0x10
+	cmp r1, #0x0
+	bne _020DC7F4
+_020DC84C:
+	mov r0, r12
+	add sp, sp, #0x24
+	ldmia sp!, {r4-r9,lr}
+	bx lr
+	.balign 4
+_020DC85C: .word 0x000080FF
+_020DC860: .word 0x0210400C
+_020DC864: .word 0x021D6B08
+_020DC868: .word 0x02104048
+
+	arm_func_start CTRDGi_WriteFlashSectorCoreAT
+CTRDGi_WriteFlashSectorCoreAT: ; 0x020DC86C
+	stmdb sp!, {r4-r5,lr}
+	sub sp, sp, #0x24
+	mov r5, r0
+	add r4, sp, #0x0
+	ldmia r5!, {r0-r3}
+	stmia r4!, {r0-r3}
+	ldmia r5!, {r0-r3}
+	stmia r4!, {r0-r3}
+	ldr r0, _020DC984 ; =0x021D6B0C
+	ldr r1, [r5, #0x0]
+	ldrh r0, [r0, #0x0]
+	str r1, [r4, #0x0]
+	ldr r4, [sp, #0xc]
+	bl OS_LockCartridge
+	ldr r5, _020DC988 ; =0x04000204
+	ldr r1, _020DC98C ; =0x021D6B20
+	ldrh r0, [r5, #0x0]
+	ldr r1, [r1, #0x0]
+	ldrh r2, [r5, #0x0]
+	ldr r3, [r1, #0x10]
+	ldr r1, _020DC990 ; =0x02104048
+	bic r2, r2, #0x3
+	orr r2, r2, r3
+	strh r2, [r5, #0x0]
+	ldr r5, _020DC994 ; =0x04000208
+	ldrh lr, [sp, #0x20]
+	ldrh r12, [r1, #0x28]
+	ldrh r2, [r5, #0x0]
+	mov r3, #0x0
+	mov lr, lr, lsl r12
+	strh r3, [r5, #0x0]
+	ldr ip, _020DC998 ; =0x0A005555
+	mov r5, #0xaa
+	strb r5, [r12, #0x0]
+	ldr r3, _020DC99C ; =0x0A002AAA
+	mov r5, #0x55
+	strb r5, [r3, #0x0]
+	mov r3, #0xa0
+	strb r3, [r12, #0x0]
+	ldr r3, [r1, #0x24]
+	and r5, r0, #0x3
+	cmp r3, #0x0
+	add r1, lr, #0xa000000
+	beq _020DC92C
+_020DC91C:
+	ldrb r0, [r4], #0x1
+	subs r3, r3, #0x1
+	strb r0, [r1], #0x1
+	bne _020DC91C
+_020DC92C:
+	ldr ip, _020DC994 ; =0x04000208
+	ldr r3, _020DC9A0 ; =0x021D6B24
+	ldrh r0, [r12, #0x0]
+	sub r1, r1, #0x1
+	mov r0, #0x1
+	strh r2, [r12, #0x0]
+	ldrb r2, [r4, #-0x1]
+	ldr r3, [r3, #0x0]
+	blx r3
+	ldr r3, _020DC988 ; =0x04000204
+	ldr r1, _020DC984 ; =0x021D6B0C
+	ldrh r2, [r3, #0x0]
+	mov r4, r0
+	bic r0, r2, #0x3
+	orr r0, r0, r5
+	strh r0, [r3, #0x0]
+	ldrh r0, [r1, #0x0]
+	bl OS_UnlockCartridge2
+	mov r0, r4
+	add sp, sp, #0x24
+	ldmia sp!, {r4-r5,lr}
+	bx lr
+	.balign 4
+_020DC984: .word 0x021D6B0C
+_020DC988: .word 0x04000204
+_020DC98C: .word 0x021D6B20
+_020DC990: .word 0x02104048
+_020DC994: .word 0x04000208
+_020DC998: .word 0x0A005555
+_020DC99C: .word 0x0A002AAA
+_020DC9A0: .word 0x021D6B24
+
+	arm_func_start CTRDGi_EraseFlash4KBCoreAT
+CTRDGi_EraseFlash4KBCoreAT: ; 0x020DC9A4
+	stmdb sp!, {r4-r7,lr}
+	sub sp, sp, #0x24
+	mov r5, r0
+	ldmia r5!, {r0-r3}
+	add r4, sp, #0x0
+	stmia r4!, {r0-r3}
+	ldmia r5!, {r0-r3}
+	stmia r4!, {r0-r3}
+	ldr r0, [r5, #0x0]
+	str r0, [r4, #0x0]
+	ldrh r0, [sp, #0x20]
+	cmp r0, #0x10
+	addcs sp, sp, #0x24
+	ldrhs r0, _020DCA54 ; =0x000080FF
+	ldmcsia sp!, {r4-r7,lr}
+	bxcs lr
+	mov r0, r0, lsl #0x15
+	mov r5, r0, lsr #0x10
+	mov r6, #0x0
+	mov r4, #0x2
+_020DC9F4:
+	mov r7, r4
+_020DC9F8:
+	mov r0, r5
+	bl CTRDGi_EraseFlashSectorAT
+_020DCA00: ; 0x020DCA00
+	cmp r0, #0x0
+	beq _020DCA18
+	sub r1, r7, #0x1
+	mov r1, r1, lsl #0x10
+	movs r7, r1, lsr #0x10
+	bne _020DC9F8
+_020DCA18:
+	add r1, r5, #0x1
+	mov r1, r1, lsl #0x10
+	cmp r0, #0x0
+	mov r5, r1, lsr #0x10
+	addne sp, sp, #0x24
+	ldmneia sp!, {r4-r7,lr}
+	bxne lr
+	add r1, r6, #0x1
+	mov r1, r1, lsl #0x10
+	mov r6, r1, lsr #0x10
+	cmp r6, #0x20
+	blo _020DC9F4
+	add sp, sp, #0x24
+	ldmia sp!, {r4-r7,lr}
+	bx lr
+	.balign 4
+_020DCA54: .word 0x000080FF
+
+	arm_func_start CTRDGi_EraseFlashSectorCoreAT
+CTRDGi_EraseFlashSectorCoreAT: ; 0x020DCA58
+	stmdb sp!, {r4-r5,lr}
+	sub sp, sp, #0x24
+	mov r5, r0
+	ldmia r5!, {r0-r3}
+	add r4, sp, #0x0
+	stmia r4!, {r0-r3}
+	ldmia r5!, {r0-r3}
+	stmia r4!, {r0-r3}
+	ldr r0, [r5, #0x0]
+	ldr r1, _020DCB80 ; =0x02104048
+	str r0, [r4, #0x0]
+	ldr r0, _020DCB84 ; =0x021D6B0C
+	ldrh r2, [sp, #0x20]
+	ldrh r1, [r1, #0x28]
+	ldrh r0, [r0, #0x0]
+	mov r1, r2, lsl r1
+	add r5, r1, #0xa000000
+	bl OS_LockCartridge
+	ldr r4, _020DCB88 ; =0x04000204
+	ldr r1, _020DCB8C ; =0x021D6B20
+	ldrh r0, [r4, #0x0]
+	ldr r2, [r1, #0x0]
+	ldrh r1, [r4, #0x0]
+	ldr r2, [r2, #0x10]
+	ldr r3, _020DCB90 ; =0x04000208
+	bic r1, r1, #0x3
+	orr r1, r1, r2
+	strh r1, [r4, #0x0]
+	ldrh r2, [r3, #0x0]
+	mov r1, #0x0
+	ldr r4, _020DCB94 ; =0x0A005555
+	strh r1, [r3, #0x0]
+	mov r3, #0xaa
+	strb r3, [r4, #0x0]
+	ldr r1, _020DCB98 ; =0x0A002AAA
+	mov r3, #0x55
+	strb r3, [r1, #0x0]
+	mov r3, #0xa0
+	strb r3, [r4, #0x0]
+	ldr r1, _020DCB80 ; =0x02104048
+	and r4, r0, #0x3
+	ldr r1, [r1, #0x24]
+	cmp r1, #0x0
+	beq _020DCB18
+	mov r0, #0xff
+_020DCB0C:
+	strb r0, [r5], #0x1
+	subs r1, r1, #0x1
+	bne _020DCB0C
+_020DCB18:
+	ldr ip, _020DCB90 ; =0x04000208
+	ldr r3, _020DCB9C ; =0x021D6B24
+	ldrh r0, [r12, #0x0]
+	sub r1, r5, #0x1
+	strh r2, [r12, #0x0]
+	ldr r3, [r3, #0x0]
+	mov r0, #0x1
+	mov r2, #0xff
+	blx r3
+	ldr r2, _020DCB88 ; =0x04000204
+	movs r5, r0
+	ldrh r1, [r2, #0x0]
+	andne r0, r5, #0xff00
+	orrne r0, r0, #0x2
+	movne r0, r0, lsl #0x10
+	bic r1, r1, #0x3
+	movne r5, r0, lsr #0x10
+	orr r1, r1, r4
+	ldr r0, _020DCB84 ; =0x021D6B0C
+	strh r1, [r2, #0x0]
+	ldrh r0, [r0, #0x0]
+	bl OS_UnlockCartridge2
+	mov r0, r5
+	add sp, sp, #0x24
+	ldmia sp!, {r4-r5,lr}
+	bx lr
+	.balign 4
+_020DCB80: .word 0x02104048
+_020DCB84: .word 0x021D6B0C
+_020DCB88: .word 0x04000204
+_020DCB8C: .word 0x021D6B20
+_020DCB90: .word 0x04000208
+_020DCB94: .word 0x0A005555
+_020DCB98: .word 0x0A002AAA
+_020DCB9C: .word 0x021D6B24
+
+	arm_func_start CTRDGi_EraseFlashChipCoreAT
+CTRDGi_EraseFlashChipCoreAT: ; 0x020DCBA0
+	stmdb sp!, {r4-r5,lr}
+	sub sp, sp, #0x4
+	ldr r0, _020DCC7C ; =0x021D6B0C
+	ldrh r0, [r0, #0x0]
+	bl OS_LockCartridge
+	ldr r4, _020DCC80 ; =0x04000204
+	ldr r0, _020DCC84 ; =0x021D6B20
+	ldrh r3, [r4, #0x0]
+	ldr r0, [r0, #0x0]
+	ldrh r1, [r4, #0x0]
+	ldr r2, [r0, #0x10]
+	ldr r0, _020DCC88 ; =0x04000208
+	bic r1, r1, #0x3
+	orr r1, r1, r2
+	strh r1, [r4, #0x0]
+	ldrh r4, [r0, #0x0]
+	mov r2, #0x0
+	ldr r1, _020DCC8C ; =0x021D6B38
+	strh r2, [r0, #0x0]
+	ldr lr, _020DCC90 ; =0x0A005555
+	str r4, [r1, #0x0]
+	mov r5, #0xaa
+	ldr r4, _020DCC94 ; =0x0A002AAA
+	strb r5, [lr, #0x0]
+	mov r12, #0x55
+	strb r12, [r4, #0x0]
+	mov r2, #0x80
+	strb r2, [lr, #0x0]
+	strb r5, [lr, #0x0]
+	strb r12, [r4, #0x0]
+	mov r2, #0x10
+	strb r2, [lr, #0x0]
+	ldrh r2, [r0, #0x0]
+	ldr r2, [r1, #0x0]
+	ldr r1, _020DCC98 ; =0x021D6B24
+	strh r2, [r0, #0x0]
+	ldr r4, [r1, #0x0]
+	mov r0, #0x3
+	mov r1, #0xa000000
+	mov r2, #0xff
+	and r5, r3, #0x3
+	blx r4
+	ldr r3, _020DCC80 ; =0x04000204
+	ldr r1, _020DCC7C ; =0x021D6B0C
+	ldrh r2, [r3, #0x0]
+	mov r4, r0
+	bic r0, r2, #0x3
+	orr r0, r0, r5
+	strh r0, [r3, #0x0]
+	ldrh r0, [r1, #0x0]
+	bl OS_UnlockCartridge2
+	mov r0, r4
+	add sp, sp, #0x4
+	ldmia sp!, {r4-r5,lr}
+	bx lr
+	.balign 4
+_020DCC7C: .word 0x021D6B0C
+_020DCC80: .word 0x04000204
+_020DCC84: .word 0x021D6B20
+_020DCC88: .word 0x04000208
+_020DCC8C: .word 0x021D6B38
+_020DCC90: .word 0x0A005555
+_020DCC94: .word 0x0A002AAA
+_020DCC98: .word 0x021D6B24
---
+++ arm9/asm/CTRDG_proc.s
@@ -0,0 +1,285 @@
+	.include "asm/macros.inc"
+	.include "global.inc"
+
+    .text
+
+	arm_func_start CTRDGi_CallbackForSetPhi
+CTRDGi_CallbackForSetPhi: ; 0x020DB978
+	ldr r0, _020DB988 ; =0x021D694C
+	mov r1, #0x0
+	str r1, [r0, #0x0]
+	bx lr
+	.balign 4
+_020DB988: .word 0x021D694C
+
+	arm_func_start CTRDG_TerminateForPulledOut
+CTRDG_TerminateForPulledOut: ; 0x020DB98C
+	stmdb sp!, {lr}
+	sub sp, sp, #0x4
+	mov r0, #0x2
+	bl CTRDGi_SendtoPxi
+	bl OS_Terminate
+	add sp, sp, #0x4
+	ldmia sp!, {lr}
+	bx lr
+
+	arm_func_start CTRDGi_PulledOutCallback
+CTRDGi_PulledOutCallback: ; 0x020DB9AC
+	stmdb sp!, {lr}
+	sub sp, sp, #0x4
+	and r0, r1, #0x3f
+	cmp r0, #0x11
+	bne _020DBA14
+	ldr r0, _020DBA24 ; =0x021D6954
+	ldr r0, [r0, #0x0]
+	cmp r0, #0x0
+	addne sp, sp, #0x4
+	ldmneia sp!, {lr}
+	bxne lr
+	ldr r1, _020DBA28 ; =0x021D6958
+	mov r0, #0x0
+	ldr r1, [r1, #0x0]
+	cmp r1, #0x0
+	beq _020DB9F0
+	blx r1
+_020DB9F0:
+	cmp r0, #0x0
+	beq _020DB9FC
+	bl CTRDG_TerminateForPulledOut
+_020DB9FC:
+	ldr r0, _020DBA24 ; =0x021D6954
+	mov r1, #0x1
+	str r1, [r0, #0x0]
+	add sp, sp, #0x4
+	ldmia sp!, {lr}
+	bx lr
+_020DBA14:
+	bl OS_Terminate
+	add sp, sp, #0x4
+	ldmia sp!, {lr}
+	bx lr
+	.balign 4
+_020DBA24: .word 0x021D6954
+_020DBA28: .word 0x021D6958
+
+	arm_func_start CTRDGi_CallbackForInitModuleInfo
+CTRDGi_CallbackForInitModuleInfo: ; 0x020DBA2C
+	stmdb sp!, {lr}
+	sub sp, sp, #0x4
+	and r0, r1, #0x3f
+	cmp r0, #0x1
+	ldreq r0, _020DBA64 ; =0x021D6944
+	moveq r1, #0x1
+	streqh r1, [r0, #0x0]
+	addeq sp, sp, #0x4
+	ldmeqia sp!, {lr}
+	bxeq lr
+	bl OS_Terminate
+	add sp, sp, #0x4
+	ldmia sp!, {lr}
+	bx lr
+	.balign 4
+_020DBA64: .word 0x021D6944
+
+	arm_func_start CTRDGi_InitModuleInfo
+CTRDGi_InitModuleInfo: ; 0x020DBA68
+	stmdb sp!, {r4-r7,lr}
+	sub sp, sp, #0x14
+	ldr r0, _020DBC50 ; =0x021D6948
+	ldr r1, [r0, #0x0]
+	cmp r1, #0x0
+	addne sp, sp, #0x14
+	ldmneia sp!, {r4-r7,lr}
+	bxne lr
+	ldr r1, _020DBC54 ; =0x04000300
+	mov r2, #0x1
+	str r2, [r0, #0x0]
+	ldrh r0, [r1, #0x0]
+	ands r0, r0, #0x1
+	addeq sp, sp, #0x14
+	ldmeqia sp!, {r4-r7,lr}
+	bxeq lr
+	mov r0, #0x40000
+	bl OS_SetIrqMask
+	ldr r3, _020DBC58 ; =0x04000208
+	mov r2, #0x1
+	ldrh r4, [r3, #0x0]
+	ldr r1, _020DBC5C ; =0x021D6944
+	mov r6, r0
+	strh r2, [r3, #0x0]
+	ldrh r0, [r1, #0x2]
+	add r1, sp, #0x0
+	bl CTRDGi_LockByProcessor
+	ldr r1, _020DBC60 ; =0x04000204
+	add r0, sp, #0x8
+	ldrh r1, [r1, #0x0]
+	and r1, r1, #0x8000
+	mov r5, r1, asr #0xf
+	bl CTRDGi_ChangeLatestAccessCycle
+	ldr r3, _020DBC60 ; =0x04000204
+	ldr r0, _020DBC64 ; =0x021D6960
+	ldrh r2, [r3, #0x0]
+	add r0, r0, #0x80
+	mov r1, #0x40
+	bic r2, r2, #0x8000
+	strh r2, [r3, #0x0]
+	bl DC_InvalidateRange
+	ldr r2, _020DBC64 ; =0x021D6960
+	ldr r1, _020DBC68 ; =0x08000080
+	mov r0, #0x1
+	mov r3, #0x40
+	add r2, r2, #0x80
+	bl MI_DmaCopy16
+	ldr r2, _020DBC60 ; =0x04000204
+	add r0, sp, #0x8
+	ldrh r1, [r2, #0x0]
+	bic r1, r1, #0x8000
+	orr r1, r1, r5, lsl #0xf
+	strh r1, [r2, #0x0]
+	bl CTRDGi_RestoreAccessCycle
+	ldr r0, _020DBC5C ; =0x021D6944
+	add r1, sp, #0x0
+	ldrh r0, [r0, #0x2]
+	bl CTRDGi_UnlockByProcessor
+	ldr r0, _020DBC6C ; =0x027FFF9B
+	ldrb r0, [r0, #0x0]
+	cmp r0, #0x0
+	bne _020DBB70
+	ldr r0, _020DBC70 ; =0x027FFF9A
+	ldrb r0, [r0, #0x0]
+	cmp r0, #0x0
+	bne _020DBBDC
+_020DBB70:
+	ldr r2, _020DBC64 ; =0x021D6960
+	ldr r0, _020DBC74 ; =0x027FFC30
+	ldrh r1, [r2, #0xbe]
+	mov r3, #0x0
+	strh r1, [r0, #0x0]
+_020DBB84:
+	add r0, r2, r3
+	ldrb r1, [r0, #0xb5]
+	add r0, r3, #0x2700000
+	add r0, r0, #0xff000
+	add r3, r3, #0x1
+	strb r1, [r0, #0xC32]
+	cmp r3, #0x3
+	blt _020DBB84
+	ldrh r0, [r2, #0xb0]
+	ldr r1, _020DBC74 ; =0x027FFC30
+	strh r0, [r1, #0x6]
+	ldr r0, [r2, #0xac]
+	str r0, [r1, #0x8]
+	bl CTRDG_IsExisting
+_020DBBBC: ; 0x020DBBBC
+	cmp r0, #0x0
+	movne r2, #0x1
+	ldr r1, _020DBC6C ; =0x027FFF9B
+	moveq r2, #0x0
+	strb r2, [r1, #0x0]
+	ldr r0, _020DBC70 ; =0x027FFF9A
+	mov r1, #0x1
+	strb r1, [r0, #0x0]
+_020DBBDC:
+	ldr r0, _020DBC78 ; =0xFFFF0020
+	ldr r1, _020DBC7C ; =0x021D6964
+	mov r2, #0x9c
+	bl MIi_CpuCopy32
+	bl DC_FlushAll
+	ldr r0, _020DBC64 ; =0x021D6960
+	add r0, r0, #0xfe000000
+	mov r0, r0, lsr #0x5
+	mov r0, r0, lsl #0x6
+	orr r0, r0, #0x1
+	bl CTRDGi_SendtoPxi
+	ldr r5, _020DBC5C ; =0x021D6944
+	ldrh r0, [r5, #0x0]
+	cmp r0, #0x1
+	beq _020DBC30
+	mov r7, #0x1
+_020DBC1C:
+	mov r0, r7
+	bl SVC_WaitByLoop
+	ldrh r0, [r5, #0x0]
+	cmp r0, #0x1
+	bne _020DBC1C
+_020DBC30:
+	ldr r2, _020DBC58 ; =0x04000208
+	mov r0, r6
+	ldrh r1, [r2, #0x0]
+	strh r4, [r2, #0x0]
+	bl OS_SetIrqMask
+	add sp, sp, #0x14
+	ldmia sp!, {r4-r7,lr}
+	bx lr
+	.balign 4
+_020DBC50: .word 0x021D6948
+_020DBC54: .word 0x04000300
+_020DBC58: .word 0x04000208
+_020DBC5C: .word 0x021D6944
+_020DBC60: .word 0x04000204
+_020DBC64: .word 0x021D6960
+_020DBC68: .word 0x08000080
+_020DBC6C: .word 0x027FFF9B
+_020DBC70: .word 0x027FFF9A
+_020DBC74: .word 0x027FFC30
+_020DBC78: .word 0xFFFF0020
+_020DBC7C: .word 0x021D6964
+
+	arm_func_start CTRDG_Init
+CTRDG_Init: ; 0x020DBC80
+	stmdb sp!, {r4-r5,lr}
+	sub sp, sp, #0x4
+	ldr r0, _020DBD34 ; =0x021D6950
+	ldr r1, [r0, #0x0]
+	cmp r1, #0x0
+	addne sp, sp, #0x4
+	ldmneia sp!, {r4-r5,lr}
+	bxne lr
+	mov r1, #0x1
+	str r1, [r0, #0x0]
+	bl CTRDGi_InitCommon
+	ldr r0, _020DBD38 ; =0x021D6954
+	mov r1, #0x0
+	str r1, [r0, #0x0]
+	bl PXI_Init
+	mov r5, #0xd
+	mov r4, #0x1
+_020DBCC4:
+	mov r0, r5
+	mov r1, r4
+	bl PXI_IsCallbackReady
+_020DBCD0: ; 0x020DBCD0
+	cmp r0, #0x0
+	beq _020DBCC4
+	ldr r1, _020DBD3C ; =CTRDGi_CallbackForInitModuleInfo
+	mov r0, #0xd
+	bl PXI_SetFifoRecvCallback
+	bl CTRDGi_InitModuleInfo
+	mov r0, #0xd
+	mov r1, #0x0
+	bl PXI_SetFifoRecvCallback
+	ldr r1, _020DBD40 ; =CTRDGi_PulledOutCallback
+	mov r0, #0xd
+	bl PXI_SetFifoRecvCallback
+	ldr r1, _020DBD44 ; =0x021D6958
+	mov r2, #0x0
+	ldr r0, _020DBD48 ; =0x021D6A20
+	str r2, [r1, #0x0]
+	bl CTRDGi_InitTaskThread
+	ldr r1, _020DBD4C ; =CTRDGi_CallbackForSetPhi
+	mov r0, #0x11
+	bl PXI_SetFifoRecvCallback
+_020DBD20: ; 0x020DBD20
+	mov r0, #0x0
+	bl CTRDG_Enable
+	add sp, sp, #0x4
+	ldmia sp!, {r4-r5,lr}
+	bx lr
+	.balign 4
+_020DBD34: .word 0x021D6950
+_020DBD38: .word 0x021D6954
+_020DBD3C: .word CTRDGi_CallbackForInitModuleInfo
+_020DBD40: .word CTRDGi_PulledOutCallback
+_020DBD44: .word 0x021D6958
+_020DBD48: .word 0x021D6A20
+_020DBD4C: .word CTRDGi_CallbackForSetPhi
