a43d02ff7a3f9ed0da04d12ca2973a3372d14344
Egor Ananyin <ananinegor@gmail.com>
2020-05-22 19:53:22 +0300

    Add arm_func

--- arm9/lib/src/MATH_dgt.c
+++ arm9/lib/src/MATH_dgt.c
@@ -1,14 +1,14 @@
 #include "global.h"
 #include "MATH_dgt.h"
 
-void MATH_CalcMD5(void *digest, const void *data, u32 dataLength) {
+ARM_FUNC void MATH_CalcMD5(void *digest, const void *data, u32 dataLength) {
     MATHMD5Context context;
     MATH_MD5Init(&context);
     MATH_MD5Update(&context, data, dataLength);
     MATH_MD5GetHash(&context, digest);
 }
 
-void MATH_CalcSHA1(void *digest, const void *data, u32 dataLength) {
+ARM_FUNC void MATH_CalcSHA1(void *digest, const void *data, u32 dataLength) {
     MATHSHA1Context context;
     MATH_SHA1Init(&context);
     MATH_SHA1Update(&context, data, dataLength);
