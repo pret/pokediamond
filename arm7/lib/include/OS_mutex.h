9fba6d2b34c67d4b14bb1a17d0b1d6d5d6d19b08
Egor Ananyin <ananinegor@gmail.com>
2020-06-20 11:00:20 +0300

    OS_mutext done!

--- arm7/lib/include/OS_mutex.h
+++ arm7/lib/include/OS_mutex.h
@@ -4,7 +4,11 @@
 #include "OS_context.h"
 #include "nitro/OS_mutex_shared.h"
 
+void OSi_DequeueItem(OSThread * thread, OSMutex * mutex);
+void OSi_EnqueueTail(OSThread * thread, OSMutex * mutex);
 void OS_InitMutex(OSMutex* mutex);
+void OS_LockMutex(OSMutex* mutex);
+void OS_UnlockMutex(OSMutex* mutex);
 void OSi_UnlockAllMutex(OSThread * thread);
 
 #endif //POKEDIAMOND_ARM7_OS_MUTEX_H
