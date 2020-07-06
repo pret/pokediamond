4d7fd9ee0c39e73b637a894b6751095e2bcd5b74
Egor Ananyin <ananinegor@gmail.com>
2020-07-06 09:42:04 +0300

    rename

---
+++ arm7/lib/include/PAD_XYButton.h
@@ -0,0 +1,8 @@
+#ifndef POKEDIAMOND_PAD_XYBUTTON_H
+#define POKEDIAMOND_PAD_XYBUTTON_H
+
+#include "nitro/types.h"
+
+BOOL PAD_InitXYButton(void);
+
+#endif
--- arm7/lib/include/pad.h
+++
@@ -1,8 +0,0 @@
-#ifndef POKEDIAMOND_PAD_H
-#define POKEDIAMOND_PAD_H
-
-#include "nitro/types.h"
-
-BOOL PAD_InitXYButton(void);
-
-#endif
