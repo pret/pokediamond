6a9e10d19e0845c34f842a51b5bb1914f5a8113b
red031000 <rubenru09@aol.com>
2020-07-09 13:21:08 +0100

    module 46 fully shiftable

--- arm9/modules/46/asm/module_46.s
+++ arm9/modules/46/asm/module_46.s
@@ -95,7 +95,7 @@ MOD46_022548D4: ; 0x022548D4
 	cmp r1, #3
 	bhs _02254900
 	lsl r2, r1, #2
-	ldr r1, _02254904 ; =0x02254AF4
+	ldr r1, _02254904 ; =MOD46_02254AF4
 	add r0, r4, #0
 	ldr r1, [r1, r2]
 	blx r1
@@ -110,7 +110,7 @@ MOD46_022548D4: ; 0x022548D4
 _02254900:
 	pop {r3, r4, r5, pc}
 	nop
-_02254904: .word 0x02254AF4
+_02254904: .word MOD46_02254AF4
 	thumb_func_end MOD46_022548D4
 
 	thumb_func_start MOD46_02254908
@@ -264,13 +264,13 @@ MOD46_022549E8: ; 0x022549E8
 	str r0, [sp, #4]
 	mov r0, #8
 	str r0, [sp, #8]
-	ldr r0, _02254A08 ; =0x02254B1C
+	ldr r0, _02254A08 ; =MOD46_02254B1C
 	ldr r3, [r2]
 	bl MOD20_022537E0
 	add sp, #0xc
 	pop {pc}
 	nop
-_02254A08: .word 0x02254B1C
+_02254A08: .word MOD46_02254B1C
 	thumb_func_end MOD46_022549E8
 
 	thumb_func_start MOD46_02254A0C
@@ -312,7 +312,7 @@ MOD46_02254A38: ; 0x02254A38
 	bl MOD20_022538A0
 	add r5, r0, #0
 	ldr r0, [r5, #4]
-	ldr r2, _02254ACC ; =0x02254B00
+	ldr r2, _02254ACC ; =MOD46_02254B00
 	mov r1, #6
 	mov r3, #0
 	bl FUN_02016C18
@@ -371,7 +371,7 @@ MOD46_02254A38: ; 0x02254A38
 	add sp, #0x14
 	pop {r4, r5, pc}
 	nop
-_02254ACC: .word 0x02254B00
+_02254ACC: .word MOD46_02254B00
 _02254AD0: .word 0x04001000
 _02254AD4: .word 0xFFFFE0FF
 	thumb_func_end MOD46_02254A38
@@ -392,12 +392,22 @@ MOD46_02254AD8: ; 0x02254AD8
 	thumb_func_end MOD46_02254AD8
 
 	.section .rodata
-	; 0x02254AF4
-	.byte 0x25, 0x49, 0x25, 0x02, 0x65, 0x49, 0x25, 0x02, 0x79, 0x49, 0x25, 0x02, 0x00, 0x00, 0x00, 0x00
+
+	.global MOD46_02254AF4
+MOD46_02254AF4: ; 0x02254AF4
+	.word MOD46_02254924, MOD46_02254964, MOD46_02254978
+
+	.global MOD46_02254B00
+MOD46_02254B00: ; 0x02254B00
+	.byte 0x00, 0x00, 0x00, 0x00
 	.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x0E, 0x00
-	.byte 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x39, 0x4A, 0x25, 0x02
-	.byte 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0xD9, 0x4A, 0x25, 0x02, 0x00, 0x00, 0x00, 0x00
-	.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	.byte 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+	.global MOD46_02254B1C
+MOD46_02254B1C: ; 0x02254B1C
+	.word 0x00, MOD46_02254A38, 0x00
+	.word 0x01, MOD46_02254AD8, 0x00
+	.word 0x00, 0x00000000, 0x00
 
 	.section .sinit
 	.word MOD46_02254840
