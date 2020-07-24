c355575f3954a19dfed6c350a8ad34175263da46
red031000 <rubenru09@aol.com>
2020-07-24 22:52:51 +0100

    annotate MCR and MRC

--- arm9/lib/src/OS_protectionUnit.c
+++ arm9/lib/src/OS_protectionUnit.c
@@ -1,22 +1,18 @@
-//
-// Created by red031000 on 2020-05-24.
-//
-
 #include "function_target.h"
 #include "OS_protectionUnit.h"
 
 ARM_FUNC asm void OS_EnableProtectionUnit(void)
 {
-    mrc p15, 0x0, r0, c1, c0, 0x0
+    mrc p15, 0x0, r0, c1, c0, 0x0 //Control Register
     orr r0, r0, #0x1
-    mcr p15, 0x0, r0, c1, c0, 0x0
+    mcr p15, 0x0, r0, c1, c0, 0x0 //Control Register
     bx lr
 }
 
 ARM_FUNC asm void OS_DisableProtectionUnit(void)
 {
-    mrc p15, 0x0, r0, c1, c0, 0x0
+    mrc p15, 0x0, r0, c1, c0, 0x0 //Control Register
     bic r0, r0, #0x1
-    mcr p15, 0x0, r0, c1, c0, 0x0
+    mcr p15, 0x0, r0, c1, c0, 0x0 //Control Register
     bx lr
 }
--- arm9/lib/src/OS_terminate_proc.c
+++ arm9/lib/src/OS_terminate_proc.c
@@ -11,6 +11,6 @@ ARM_FUNC void OS_Terminate(void) {
 
 ARM_FUNC asm void OS_Halt(void) {
     mov r0, #0x0
-    mcr p15, 0x0, r0, c7, c0, 0x4
+    mcr p15, 0x0, r0, c7, c0, 0x4 //Wait For Interrupt (Halt)
     bx lr
 }
