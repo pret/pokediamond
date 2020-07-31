43e0700005cd97bb514633b2fe914bf33921cc7e
red031000 <rubenru09@aol.com>
2020-07-31 20:13:41 +0100

    mod63_021DB838

--- arm9/modules/63/src/mod63_021DB450.c
+++ arm9/modules/63/src/mod63_021DB450.c
@@ -41,8 +41,6 @@ extern BOOL FUN_020133C8(u32);
 extern void FUN_020079E0(u32, u32, s32);
 
 //todo funcs
-extern BOOL MOD63_021DB7D0(struct UnkStruct63_021DB5CC *, u32);
-extern BOOL MOD63_021DB838(struct UnkStruct63_021DB5CC *, u32);
 extern BOOL MOD63_021DB884(struct UnkStruct63_021DB5CC *, u32);
 extern BOOL MOD63_021DB8E8(struct UnkStruct63_021DB5CC *, u32);
 extern void MOD63_021DB940();
@@ -233,6 +231,21 @@ THUMB_FUNC BOOL MOD63_021DB7D0(UnkStruct63_021DB5CC *param0, u32 param1)
     FUN_020079E0(field, 0x1, -0x10);
     FUN_020079E0(field, 0xC, -0x80);
     FUN_020079E0(field, 0xD, -0x80);
+    param0->field_1D++;
+    if (param0->field_1D >= 6)
+    {
+        FUN_02007558(field, 0xC, 0x100);
+        FUN_02007558(field, 0xD, 0x100);
+        return TRUE;
+    }
+    return FALSE;
+}
+
+THUMB_FUNC BOOL MOD63_021DB838(UnkStruct63_021DB5CC *param0, u32 param1)
+{
+    u32 field = param0->field_04[param1];
+    FUN_020079E0(field, 0, -0x6);
+    FUN_020079E0(field, 1, -0x10);
     param0->field_1D++;
     if (param0->field_1D >= 6)
     {
