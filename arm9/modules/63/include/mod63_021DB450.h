feaa5cf511ec56fa1a51c87db388b9d0cccaad80
red031000 <rubenru09@aol.com>
2020-08-01 12:40:50 +0100

    merge several structs

--- arm9/modules/63/include/mod63_021DB450.h
+++ arm9/modules/63/include/mod63_021DB450.h
@@ -3,22 +3,31 @@
 
 #include "global.h"
 
-typedef struct UnkStruct63_021DB49C //animation struct?
+typedef struct UnkStruct63_021DB450
 {
-    u32 field_00;
-    u32 field_04[3]; //animation array?
+    struct UnkStruct63_021DB450 * field_00;
+    u32 field_04[3];
     u32 field_10;
     void * field_14;
     void * field_18;
-} UnkStruct63_021DB49C;
+    u8 field_1C;
+    u8 field_1D;
+    u8 padding_1E[2];
+    u32 ** field_20;
+    u32 field_24;
+    u32 field_28;
+    u32 field_2C;
+    u32 field_30;
+    u8 filler_34[0x30];
+} UnkStruct63_021DB450;
 
-typedef struct UnkStruct63_021DB49C_2
+typedef struct UnkStruct63_021DB49C
 {
     u32 field_00;
     u32 field_04;
     u32 field_08;
     u32 field_0C;
-} UnkStruct63_021DB49C_2;
+} UnkStruct63_021DB49C;
 
 typedef struct UnkStruct63_021DBEF0
 {
@@ -26,7 +35,7 @@ typedef struct UnkStruct63_021DBEF0
     u32 field_04;
 } UnkStruct63_021DBEF0;
 
-typedef struct UnkStruct63_021DB580 //possibly part of the above structs?
+typedef struct UnkStruct63_021DB580
 {
     u32 field_00;
     u8 filler[0x10];
@@ -34,63 +43,27 @@ typedef struct UnkStruct63_021DB580 //possibly part of the above structs?
     u32 field_24;
 } UnkStruct63_021DB580;
 
-typedef struct UnkStruct63_021DB598 //part of the above?
-{
-    u32 field_00;
-} UnkStruct63_021DB598;
-
-typedef struct UnkStruct63_021DB5A8 //possibly the same as above
-{
-    u32 field_00;
-} UnkStruct63_021DB5A8;
-
-typedef struct UnkStruct63_021DB5CC
-{
-    u8 filler_00[4];
-    u32 field_04[5];
-    u32 field_18;
-    u8 field_1C;
-    u8 field_1D;
-} UnkStruct63_021DB5CC;
-
 typedef struct UnkStruct63_021DBED8
 {
-    BOOL (*unk_00)(struct UnkStruct63_021DB5CC *, u32);
-    BOOL (*unk_04)(struct UnkStruct63_021DB5CC *, u32);
+    BOOL (*unk_00)(struct UnkStruct63_021DB450 *, u32);
+    BOOL (*unk_04)(struct UnkStruct63_021DB450 *, u32);
 } UnkStruct63_021DBED8;
-
-typedef struct UnkStruct_02090A58
-{
-    struct UnkStruct_02090A58 * unk_00;
-    u32 unk_04;
-    u32 unk_08;
-    u32 unk_0C;
-    u32 unk_10;
-    u32 unk_14;
-    u32 unk_18;
-    u32 unk_1C;
-    u32 ** unk_20;
-    u32 unk_24;
-    u32 unk_28;
-    u32 unk_2C;
-    u32 unk_30;
-} UnkStruct_02090A58;
 
 u32 MOD63_021DB450(u32 param0, u32 param1);
 u32 MOD63_021DB474(u32 param0, u32 param1);
 u32 MOD63_021DB498(void);
-void Title_SetupMonAnimationSprites(struct UnkStruct63_021DB49C * arg0);
+void Title_SetupMonAnimationSprites(struct UnkStruct63_021DB450 * arg0);
 void MOD63_021DB580(UnkStruct63_021DB580 *param0);
-void MOD63_021DB598(UnkStruct63_021DB598 *param0);
-void MOD63_021DB5A8(UnkStruct63_021DB5A8 *param0);
-BOOL MOD63_021DB5CC(UnkStruct63_021DB5CC *param0, u32 param1, s32 param2);
-BOOL MOD63_021DB720(UnkStruct63_021DB5CC *param0, u32 param1);
-BOOL MOD63_021DB784(UnkStruct63_021DB5CC *param0, u32 param1);
-BOOL MOD63_021DB7D0(UnkStruct63_021DB5CC *param0, u32 param1);
-BOOL MOD63_021DB838(UnkStruct63_021DB5CC *param0, u32 param1);
-BOOL MOD63_021DB884(UnkStruct63_021DB5CC *param0, u32 param1);
-BOOL MOD63_021DB8E8(UnkStruct63_021DB5CC *param0, u32 param1);
+void MOD63_021DB598(UnkStruct63_021DB450 *param0);
+void MOD63_021DB5A8(UnkStruct63_021DB450 *param0);
+BOOL MOD63_021DB5CC(UnkStruct63_021DB450 *param0, u32 param1, s32 param2);
+BOOL MOD63_021DB720(UnkStruct63_021DB450 *param0, u32 param1);
+BOOL MOD63_021DB784(UnkStruct63_021DB450 *param0, u32 param1);
+BOOL MOD63_021DB7D0(UnkStruct63_021DB450 *param0, u32 param1);
+BOOL MOD63_021DB838(UnkStruct63_021DB450 *param0, u32 param1);
+BOOL MOD63_021DB884(UnkStruct63_021DB450 *param0, u32 param1);
+BOOL MOD63_021DB8E8(UnkStruct63_021DB450 *param0, u32 param1);
 void MOD63_021DB934(void);
-void MOD63_021DB940(struct UnkStruct_02090A58 * a0);
+void MOD63_021DB940(struct UnkStruct63_021DB450 * a0);
 
 #endif //POKEDIAMOND_MOD63_021DB450_H
