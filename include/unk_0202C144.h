441e8f436f0ee365596f4c20d705e8fa2b015268
ultima-soul <akshayjhanji@hotmail.com>
2020-08-27 09:40:49 -0700

    Address review comments.

--- include/unk_0202C144.h
+++ include/unk_0202C144.h
@@ -1,6 +1,6 @@
 #ifndef POKEDIAMOND_UNK_0202C144_H
 #define POKEDIAMOND_UNK_0202C144_H
 
-void FUN_0202C144(struct SaveBlock2 *, struct Pokemon *);
+void FUN_0202C144(struct SaveBlock2 * sav2, struct Pokemon * mon);
 
 #endif //POKEDIAMOND_UNK_0202C144_H
