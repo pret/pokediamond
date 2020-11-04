cf5eab9389a246354ea5a2a1f24cb281924e12ec
red031000 <rubenru09@aol.com>
2020-11-04 14:30:38 +0000

    finish mod05_021E72FC

--- arm9/modules/05/include/mod05_021E72FC.h
+++ arm9/modules/05/include/mod05_021E72FC.h
@@ -6,7 +6,8 @@
 typedef struct UnkStruct021E7358
 {
     u32 Unk00;
-    u8 dummy[8];
+    u32 Unk04;
+    u32 Unk08;
     u32 Unk0C;
     u32 Unk10;
 
@@ -15,5 +16,8 @@ typedef struct UnkStruct021E7358
 BOOL MOD05_021E72FC(u32 param0, u32 param1);
 void MOD05_021E7358(u32 param0, u32 param1, u32 param2);
 u32 MOD05_021E7388(u32 param0);
+BOOL MOD05_021E73B4(UnkStruct021E7358 *param0);
+UnkStruct021E7358 *MOD05_021E74D4(u32 param0);
+void MOD05_021E74F8(UnkStruct021E7358 *param0);
 
 #endif //POKEDIAMOND_MOD05_021E72FC_H
