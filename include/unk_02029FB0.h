f2284a721b91fc2af49bd2b5d1d43ff24de46ba6
who-knows-who <j.williams97@outlook.com>
2021-02-27 16:39:57 +0000

    Remove unk_02029FBO.h (using o instead of 0)

--- include/unk_02029FB0.h
+++ include/unk_02029FB0.h
@@ -1,9 +1,14 @@
 #ifndef POKEDIAMOND_UNK_02029FB0_H
 #define POKEDIAMOND_UNK_02029FB0_H
 
+#include "save_block_2.h"
+#include "pokedex.h"
 struct UnkStruct_02029FB0
 {
     u8 unk[0x14C];
 };
 
-#endif
+struct UnkStruct_02029FB0 * FUN_02029FC8(struct SaveBlock2 * sav2);
+void FUN_0202A1C4(struct UnkStruct_02029FB0 * unk, struct Pokedex * pokedex, u16 species);
+
+#endif //POKEDIAMOND_UNK_02029FB0_H
