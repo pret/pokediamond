1fbc3a19c26344a33dae19625a5113fe1eada50f
tgsm <doodrabbit@hotmail.com>
2021-03-24 07:21:07 -0400

    Move the MapHeader struct into map_header.h
    global.fieldmap.h is no longer used and map.h was never used, so they
    both can be removed.

--- include/map_header.h
+++ include/map_header.h
@@ -1,6 +1,29 @@
 #ifndef POKEDIAMOND_MAP_HEADER_H
 #define POKEDIAMOND_MAP_HEADER_H
 
+struct MapHeader
+{
+    u8 unk0;
+    u8 unk1;
+    u16 unk2;
+    u16 unk4;
+    u16 unk6;
+    u16 unk8;
+    u16 unkA;
+    u16 unkC;
+    u16 unkE;
+    u16 unk10;
+    u16 mapsec;
+    u8 unk14;
+    u8 unk15;
+    u8 unk16;
+    u8 unk17_0:4;
+    u8 unk17_4:1;
+    u8 unk17_5:1;
+    u8 unk17_6:1;
+    u8 unk17_7:1;
+};
+
 u8 FUN_02034724(u32 mapno);
 u8 FUN_02034738(u32 mapno);
 u16 FUN_0203474C(u32 mapno);
