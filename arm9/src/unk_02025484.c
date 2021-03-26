5e505aa880f4e07dbd20ba698d2936965f6170f1
Seth Barberee <seth.barberee@gmail.com>
2021-03-26 18:37:38 -0500

    decomp unk_02025484

---
+++ arm9/src/unk_02025484.c
@@ -0,0 +1,28 @@
+#include "global.h"
+#include "MI_memory.h"
+#include "error_handling.h"
+#include "unk_02025484.h"
+
+THUMB_FUNC void FUN_02025484(struct unk_2025484 *r0, u32 r1)
+{
+    MI_CpuFill8(r0, 0, sizeof(struct unk_2025484));
+}
+
+THUMB_FUNC u32 *FUN_02025490(struct unk_2025484 *r0, u32 r1)
+{
+    FUN_02025484(r0, r1);
+    r0->unk0 = r1;
+    return &r0->unk4;
+}
+
+THUMB_FUNC u32 *FUN_020254A0(struct unk_2025484 *r0, u32 r1)
+{
+    if(r0->unk0 != r1)
+        ErrorHandling();
+    return &r0->unk4;
+}
+
+THUMB_FUNC u32 FUN_020254B4(struct unk_2025484 *r0)
+{
+    return r0->unk0;
+}
