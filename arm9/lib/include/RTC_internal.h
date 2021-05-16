5d6018d667c8897e660ab1752b25884befdce5af
Seth Barberee <seth.barberee@gmail.com>
2021-05-15 19:47:03 -0500

    rtc funcs are all bool

--- arm9/lib/include/RTC_internal.h
+++ arm9/lib/include/RTC_internal.h
@@ -1,10 +1,10 @@
 #ifndef NITRO_RTC_INTERNAL_H_
 #define NITRO_RTC_INTERNAL_H_
 
-u32 RtcSendPxiCommand(u8 command);
-u32 RTCi_WriteRawStatus2Async(void);
-u32 RTCi_ReadRawTimeAsync(void);
-u32 RTCi_ReadRawDateAsync(void);
-u32 RTCi_ReadRawDateTimeAsync(void);
+BOOL RtcSendPxiCommand(u8 command);
+BOOL RTCi_WriteRawStatus2Async(void);
+BOOL RTCi_ReadRawTimeAsync(void);
+BOOL RTCi_ReadRawDateAsync(void);
+BOOL RTCi_ReadRawDateTimeAsync(void);
 
 #endif //NITRO_RTC_INTERNAL_H_
--- arm9/lib/src/RTC_internal.c
+++ arm9/lib/src/RTC_internal.c
@@ -2,31 +2,28 @@
 #include "PXI_fifo.h"
 #include "RTC_internal.h"
 
-u32 RTCi_ReadRawDateTimeAsync(void)
+BOOL RTCi_ReadRawDateTimeAsync(void)
 {
     return RtcSendPxiCommand(16);
 }
 
-u32 RTCi_ReadRawDateAsync(void)
+BOOL RTCi_ReadRawDateAsync(void)
 {
     return RtcSendPxiCommand(17);
 }
 
-u32 RTCi_ReadRawTimeAsync(void)
+BOOL RTCi_ReadRawTimeAsync(void)
 {
     return RtcSendPxiCommand(18);
 }
 
-u32 RTCi_WriteRawStatus2Async(void)
+BOOL RTCi_WriteRawStatus2Async(void)
 {
     return RtcSendPxiCommand(39);
 }
 
-u32 RtcSendPxiCommand(u8 command)
+BOOL RtcSendPxiCommand(u8 command)
 {
     s32 data = command << 8 & 0x7f00;
-    if(PXI_SendWordByFifo(PXI_FIFO_TAG_RTC, data, FALSE) >= 0)
-        return 1;
-    else
-        return 0;
+    return PXI_SendWordByFifo(PXI_FIFO_TAG_RTC, data, FALSE) >= 0;
 }
