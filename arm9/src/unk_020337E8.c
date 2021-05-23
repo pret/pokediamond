bc341bc17833d80eae32d8f09509b401830f54d0
Seth Barberee <seth.barberee@gmail.com>
2021-05-23 14:27:17 -0500

    address review

--- arm9/src/unk_020337E8.c
+++ arm9/src/unk_020337E8.c
@@ -5,21 +5,21 @@ extern struct DWC_Struct *FUN_020286EC(struct SaveBlock2 *sav2);
 extern struct DWC_Struct *FUN_02028228(struct DWC_Struct *);
 
 // DWC_init
-extern int DWC_Init(int);
+extern s32 DWC_Init(s32);
 
 // DWC_account
 extern void DWC_CreateExchangeToken(struct DWC_Struct *, struct DWC_token *);
-extern void DWC_CreateUserData(struct DWC_Struct *, int);
+extern void DWC_CreateUserData(struct DWC_Struct *, s32);
 extern void DWC_ClearDirtyFlag(struct DWC_Struct *);
 extern BOOL DWC_CheckHasProfile(struct DWC_Struct *);
 extern BOOL DWC_CheckValidConsole(struct DWC_Struct *);
 extern BOOL DWC_CheckUserData(struct DWC_Struct *);
-extern int DWC_GetGsProfileId(struct DWC_Struct *, struct DWC_token *);
+extern s32 DWC_GetGsProfileId(struct DWC_Struct *, struct DWC_token *);
 
-THUMB_FUNC int FUN_020337E8(u32 heap_id)
+THUMB_FUNC s32 FUN_020337E8(u32 heap_id)
 {
-    u32 ret = (u32)AllocFromHeap(heap_id, 0x720);
-    int ret1 = DWC_Init((ret + 0x1F) & ~0x1F);
+    s32 ret = (s32)AllocFromHeap(heap_id, 0x720);
+    s32 ret1 = DWC_Init((ret + 0x1F) & ~0x1F);
     FreeToHeap((void*)ret);
     return ret1;
 }
@@ -34,7 +34,7 @@ THUMB_FUNC void FUN_0203380C(struct DWC_Struct *dwc)
     }
 }
 
-THUMB_FUNC int FUN_02033830(struct DWC_Struct *dwc)
+THUMB_FUNC s32 FUN_02033830(struct DWC_Struct *dwc)
 {
     struct DWC_token token;
 
--- include/unk_020337E8.h
+++ include/unk_020337E8.h
@@ -7,7 +7,7 @@
 // Dummy struct until DWC_account
 struct DWC_Struct
 {
-    int unk0;
+    s32 unk0;
 };
 
 struct DWC_token
@@ -16,9 +16,9 @@ struct DWC_token
     u8 fill0[0xC];
 };
 
-int FUN_020337E8(u32 heap_id);
+s32 FUN_020337E8(u32 heap_id);
 void FUN_0203380C(struct DWC_Struct *);
-int FUN_02033830(struct DWC_Struct *);
+s32 FUN_02033830(struct DWC_Struct *);
 BOOL FUN_0203384C(struct SaveBlock2 *);
 
 
