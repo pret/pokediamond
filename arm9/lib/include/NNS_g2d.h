1521f64f6f055c9fd236a9e2c72ffe3860c5eb14
PikalaxALT <pikalaxalt@gmail.com>
2021-05-28 08:47:42 -0400

    FInish gf_gfx_loader.c

--- arm9/lib/include/NNS_g2d.h
+++ arm9/lib/include/NNS_g2d.h
@@ -53,6 +53,17 @@ typedef enum NNS_G2D_VRAM_TYPE
 
 } NNS_G2D_VRAM_TYPE;
 
+typedef enum NNSG2dCharacterDataMapingType
+{
+    NNS_G2D_CHARACTERMAPING_1D_32,
+    NNS_G2D_CHARACTERMAPING_1D_64,
+    NNS_G2D_CHARACTERMAPING_1D_128,
+    NNS_G2D_CHARACTERMAPING_1D_256,
+    NNS_G2D_CHARACTERMAPING_2D,
+    NNS_G2D_CHARACTERMAPING_MAX
+
+} NNSG2dCharacterDataMapingType;
+
 typedef struct NNSG2dImageAttr
 {
     GXTexSizeS                      sizeS;          // Image size (Invalid data is set during 1D mapping.)
@@ -86,7 +97,7 @@ typedef struct NNSG2dPaletteCompressInfo
     void*             pPlttIdxTbl;        // offset addr.
 
 }
-    NNSG2dPaletteCompressInfo;
+NNSG2dPaletteCompressInfo;
 
 void NNS_G2dLoadImage1DMapping
 (
@@ -114,7 +125,7 @@ typedef struct NNSG2dImagePaletteProxy
 
     NNSG2dVRamLocation        vramLocation;
 }
-    NNSG2dImagePaletteProxy;
+NNSG2dImagePaletteProxy;
 
 void NNS_G2dLoadPalette
 (
@@ -133,11 +144,121 @@ void NNS_G2dLoadPaletteEx
     NNSG2dImagePaletteProxy*            pPltProxy
 );
 
+typedef struct NNSG2dCellVramTransferData
+{
+    u32        srcDataOffset;
+    u32        szByte;
+
+}
+NNSG2dCellVramTransferData;
+
+typedef struct NNSG2dVramTransferData
+{
+    u32                                 szByteMax;             // Maximum byte count during all VRAM transfers
+    NNSG2dCellVramTransferData*         pCellTransferDataArray;// This is the length of the numCells amount of the maintained NNSG2dCellDataBank.
+    //
+
+}
+NNSG2dVramTransferData;
+
+typedef struct NNSG2dCellOAMAttrData
+{
+    u16         attr0;
+    u16         attr1;
+    u16         attr2;
+
+}
+NNSG2dCellOAMAttrData;
+
+typedef struct NNSG2dCellData
+{
+    u16                       numOAMAttrs;      // Number of OAM attributes
+    u16                       cellAttr;         // Information related to cell type
+    // Information related to flips (upper 8 bits)
+    // Information related to bounding information
+    //
+    // Information related to rendering optimization (lower 8 bits)
+    //      (6bit => Radius of bounding sphere  >> 2 )
+
+
+    NNSG2dCellOAMAttrData*    pOamAttrArray;    // Pointer to the NNSG2dCellOAMAttrData array
+
+}
+NNSG2dCellData;
+
+typedef struct NNSG2dCellDataBank
+{
+    u16                              numCells;               // Total number of cells
+    u16                              cellBankAttr;           // Attribute
+    NNSG2dCellData*                  pCellDataArrayHead;     // offset address
+    NNSG2dCharacterDataMapingType    mappingMode;            // Mapping mode of the referenced character
+
+    NNSG2dVramTransferData*          pVramTransferData;      // Information related to Vram transfer animation
+
+    void*                            pStringBank;            // Character string bank
+    void*                            pExtendedData;          // offset addr (if it exists)
+
+
+}
+NNSG2dCellDataBank;
+
+typedef enum NNSG2dAnimationPlayMode
+{
+    NNS_G2D_ANIMATIONPLAYMODE_INVALID = 0x0,    // Disabled
+    NNS_G2D_ANIMATIONPLAYMODE_FORWARD,          // one-time playback (forward)
+    NNS_G2D_ANIMATIONPLAYMODE_FORWARD_LOOP,     // repeat playback (forward loop)
+    NNS_G2D_ANIMATIONPLAYMODE_REVERSE,          // reverse playback (reverse (forward + backward))
+    NNS_G2D_ANIMATIONPLAYMODE_REVERSE_LOOP,     // reverse playback repeat (reverse (forward + backward) loop)
+    NNS_G2D_ANIMATIONPLAYMODE_MAX
+}
+NNSG2dAnimationPlayMode;
+
+typedef struct NNSG2dAnimFrameData
+{
+    void*           pContent;   // pointer to animation result
+    u16             frames;     // animation frame continuation time (units: video frames)
+    u16             pad16;      // padding
+
+}
+NNSG2dAnimFrameData;
+
+typedef struct NNSG2dAnimSequenceData
+{
+    u16                     numFrames;         // number of animation frames forming the sequence
+    u16                     loopStartFrameIdx; // loop start animation frame number
+
+    u32                     animType;          // animation type (upper 16 bits)
+    //  animation elements (lower 16 bits)
+    // (What's referenced by NNSG2dAnimFrameData.pContent is changed)
+
+    NNSG2dAnimationPlayMode playMode;          // animation sequence playback method
+    NNSG2dAnimFrameData*    pAnmFrameArray;    // offset from the head of pFrameArray.
+
+}
+NNSG2dAnimSequenceData;
+
+typedef struct NNSG2dAnimBankData
+{
+
+    u16                       numSequences;         // number of animation sequences
+    u16                       numTotalFrames;       // total number of animation frames
+    NNSG2dAnimSequenceData*   pSequenceArrayHead;   // pointer to animation sequence array
+    NNSG2dAnimFrameData*      pFrameArrayHead;      // pointer to animation frame array
+    void*                     pAnimContents;        // pointer to animation result array
+    void*                     pStringBank;          // pointer to string bank (set during execution)
+    void*                     pExtendedData;        // pointer to library expansion area (not used)
+
+}
+NNSG2dAnimBankData;
+
+
 BOOL NNS_G2dGetUnpackedBGCharacterData( void* pNcgrFile, NNSG2dCharacterData** ppCharData );
 BOOL NNS_G2dGetUnpackedCharacterData( void* pNcgrFile, NNSG2dCharacterData** ppCharData );
 BOOL NNS_G2dGetUnpackedScreenData( void* pNscrFile, NNSG2dScreenData** ppScrData );
 BOOL NNS_G2dGetUnpackedPaletteData( void* pNclrFile, NNSG2dPaletteData** ppPltData );
 BOOL NNS_G2dGetUnpackedPaletteData( void* pNclrFile, NNSG2dPaletteData** ppPltData );
 BOOL NNS_G2dGetUnpackedPaletteCompressInfo( void* pNclrFile, NNSG2dPaletteCompressInfo** ppPltCmpInfo );
+BOOL NNS_G2dGetUnpackedCellBank( void* pNcerFile, NNSG2dCellDataBank** ppCellBank );
+BOOL NNS_G2dGetUnpackedAnimBank( void* pNanrFile, NNSG2dAnimBankData** ppAnimBank );
 
 #endif //NNSYS_NNS_G2D_H
--- arm9/src/gf_gfx_loader.c
+++ arm9/src/gf_gfx_loader.c
@@ -2,6 +2,7 @@
 #include "filesystem.h"
 #include "NNS_g2d.h"
 #include "unk_02016B94.h"
+#include "GXS_ioreg.h"
 
 void * UncompressFromNarc(NarcId narcId, s32 memberNo, BOOL isCompressed, u32 heap_id, BOOL allocAtEnd);
 
@@ -146,31 +147,208 @@ THUMB_FUNC void FUN_02006A8C(NarcId narcId, s32 memberId, NNS_G2D_VRAM_TYPE r5, 
     }
 }
 
-THUMB_FUNC u32 FUN_02006AE4(NarcId narcId, s32 memberId, BOOL isCompressed, u32 r5, u32 sp18, u32 sp1C, u32 sp20, u32 heap_id, NNSG2dImageProxy *sp28)
+THUMB_FUNC u32 FUN_02006AE4(NarcId narcId, s32 memberId, BOOL isCompressed, u32 whichRoutine, u32 szByte, NNS_G2D_VRAM_TYPE type, u32 baseAddr, u32 heap_id, NNSG2dImageProxy *pImgProxy)
 {
     static void (*const UNK_020ECBA8[])(const NNSG2dCharacterData *, u32, NNS_G2D_VRAM_TYPE, NNSG2dImageProxy *) = {
         NNS_G2dLoadImage1DMapping,
         NNS_G2dLoadImage2DMapping
     };
 
-    void * r6 = UncompressFromNarc(narcId, memberId, isCompressed, heap_id, TRUE);
-    u32 r4 = 0;
-    NNSG2dCharacterData * sp4;
-    if (r6 != NULL)
+    void * pFile = UncompressFromNarc(narcId, memberId, isCompressed, heap_id, TRUE);
+    u32 retSize = 0;
+    NNSG2dCharacterData * pSrcData;
+    if (pFile != NULL)
     {
-        if (NNS_G2dGetUnpackedCharacterData(r6, &sp4))
+        if (NNS_G2dGetUnpackedCharacterData(pFile, &pSrcData))
+        {
+            if (szByte != 0)
+                pSrcData->szByte = szByte;
+            UNK_020ECBA8[whichRoutine](pSrcData, baseAddr, type, pImgProxy);
+            retSize = pSrcData->szByte;
+        }
+        FreeToHeap(pFile);
+    }
+    return retSize;
+}
+
+THUMB_FUNC void FUN_02006B38(NarcId narcId, s32 memberId, BOOL isCompressed, u32 r5, u32 sp18, NNS_G2D_VRAM_TYPE sp1C, u32 sp20, u32 heap_id, NNSG2dImageProxy * sp28)
+{
+    static void (*const UNK_020ECB98[])(const NNSG2dCharacterData *, u32, NNS_G2D_VRAM_TYPE, NNSG2dImageProxy *) = {
+        NNS_G2dLoadImage1DMapping,
+        NNS_G2dLoadImage2DMapping
+    };
+    NNSG2dCharacterData * sp4;
+    void * r4 = UncompressFromNarc(narcId, memberId, isCompressed, heap_id, TRUE);
+    if (r4 != NULL)
+    {
+        if (NNS_G2dGetUnpackedCharacterData(r4, &sp4))
         {
             if (sp18 != 0)
                 sp4->szByte = sp18;
-            UNK_020ECBA8[r5](sp4, sp20, sp1C, sp28);
-            r4 = sp4->szByte;
+            switch (sp1C)
+            {
+            case NNS_G2D_VRAM_TYPE_2DMAIN:
+                sp4->mapingType = reg_GX_DISPCNT & (REG_GX_DISPCNT_EXOBJ_CH_MASK | REG_GX_DISPCNT_OBJMAP_CH_MASK);
+                break;
+            case NNS_G2D_VRAM_TYPE_2DSUB:
+                sp4->mapingType = reg_GXS_DB_DISPCNT & (REG_GXS_DB_DISPCNT_EXOBJ_MASK | REG_GXS_DB_DISPCNT_OBJMAP_CH_MASK);
+                break;
+            default:
+                ;
+            }
+            UNK_020ECB98[r5](sp4, sp20, sp1C, sp28);
         }
-        FreeToHeap(r6);
+        FreeToHeap(r4);
     }
-    return r4;
 }
 
-void (*const UNK_020ECB98[])(const NNSG2dCharacterData *, u32, NNS_G2D_VRAM_TYPE, NNSG2dImageProxy *) = {
-    NNS_G2dLoadImage1DMapping,
-    NNS_G2dLoadImage2DMapping
-};
+THUMB_FUNC void * FUN_02006BB0(NarcId narcId, s32 memberId, BOOL isCompressed, NNSG2dCharacterData ** r4, u32 heap_id)
+{
+    void * r5 = UncompressFromNarc(narcId, memberId, isCompressed, heap_id, FALSE);
+    if (r5 != NULL)
+    {
+        if (!NNS_G2dGetUnpackedBGCharacterData(r5, r4))
+        {
+            FreeToHeap(r5);
+            return NULL;
+        }
+    }
+    return r5;
+}
+
+THUMB_FUNC void * FUN_02006BDC(NarcId narcId, s32 memberId, BOOL isCompressed, NNSG2dScreenData ** r4, u32 heap_id)
+{
+    void * r5 = UncompressFromNarc(narcId, memberId, isCompressed, heap_id, FALSE);
+    if (r5 != NULL)
+    {
+        if (!NNS_G2dGetUnpackedScreenData(r5, r4))
+        {
+            FreeToHeap(r5);
+            return NULL;
+        }
+    }
+    return r5;
+}
+
+THUMB_FUNC void * FUN_02006C08(NarcId narcId, s32 memberId, NNSG2dPaletteData ** r4, u32 heap_id)
+{
+    void * r5 = UncompressFromNarc(narcId, memberId, FALSE, heap_id, FALSE);
+    if (r5 != NULL)
+    {
+        if (!NNS_G2dGetUnpackedPaletteData(r5, r4))
+        {
+            FreeToHeap(r5);
+            return NULL;
+        }
+    }
+    return r5;
+}
+
+THUMB_FUNC void * FUN_02006C30(NarcId narcId, s32 memberId, BOOL isCompressed, NNSG2dCellDataBank ** r4, u32 heap_id)
+{
+    void * r5 = UncompressFromNarc(narcId, memberId, isCompressed, heap_id, FALSE);
+    if (r5 != NULL)
+    {
+        if (!NNS_G2dGetUnpackedCellBank(r5, r4))
+        {
+            FreeToHeap(r5);
+            return NULL;
+        }
+    }
+    return r5;
+}
+
+THUMB_FUNC void * FUN_02006C5C(NarcId narcId, s32 memberId, BOOL isCompressed, NNSG2dAnimBankData ** r4, u32 heap_id)
+{
+    void * r5 = UncompressFromNarc(narcId, memberId, isCompressed, heap_id, FALSE);
+    if (r5 != NULL)
+    {
+        if (!NNS_G2dGetUnpackedAnimBank(r5, r4))
+        {
+            FreeToHeap(r5);
+            return NULL;
+        }
+    }
+    return r5;
+}
+
+THUMB_FUNC void * FUN_02006C88(NarcId narcId, s32 memberId, u32 heap_id)
+{
+    return UncompressFromNarc(narcId, memberId, TRUE, heap_id, FALSE);
+}
+
+THUMB_FUNC void * UncompressFromNarc(NarcId narcId, s32 memberNo, BOOL isCompressed, u32 heap_id, BOOL allocAtEnd)
+{
+    void * dest;
+    void * dest2;
+    if (isCompressed || allocAtEnd == TRUE)
+    {
+        u32 size = GetNarcMemberSizeByIdPair(narcId, memberNo);
+        dest = AllocFromHeapAtEnd(heap_id, size);
+    }
+    else
+    {
+        u32 size = GetNarcMemberSizeByIdPair(narcId, memberNo);
+        dest = AllocFromHeap(heap_id, size);
+    }
+    if (dest != NULL)
+    {
+        ReadWholeNarcMemberByIdPair(dest, narcId, memberNo);
+        if (isCompressed)
+        {
+            if (allocAtEnd == FALSE)
+            {
+                dest2 = AllocFromHeap(heap_id, (*(u32 *)dest) >> 8);
+            }
+            else
+            {
+                dest2 = AllocFromHeapAtEnd(heap_id, (*(u32 *)dest) >> 8);
+            }
+            if (dest2 != NULL)
+            {
+                MI_UncompressLZ8(dest, dest2);
+                FreeToHeap(dest);
+            }
+            dest = dest2; // UB: if dest2 is NULL, dest is never freed
+        }
+    }
+    return dest;
+}
+
+THUMB_FUNC void * FUN_02006D18(NarcId narcId, s32 memberNo, BOOL isCompressed, u32 heap_id, BOOL allocAtEnd, u32 *size_p)
+{
+    void * dest;
+    void * dest2;
+    *size_p = GetNarcMemberSizeByIdPair(narcId, memberNo);
+    if (isCompressed || allocAtEnd == TRUE)
+    {
+        dest = AllocFromHeapAtEnd(heap_id, *size_p);
+    }
+    else
+    {
+        dest = AllocFromHeap(heap_id, *size_p);
+    }
+    if (dest != NULL)
+    {
+        ReadWholeNarcMemberByIdPair(dest, narcId, memberNo);
+        if (isCompressed)
+        {
+            *size_p = (*(u32 *)dest) >> 8;
+            if (allocAtEnd == FALSE)
+            {
+                dest2 = AllocFromHeap(heap_id, *size_p);
+            }
+            else
+            {
+                dest2 = AllocFromHeapAtEnd(heap_id, *size_p);
+            }
+            if (dest2 != NULL)
+            {
+                MI_UncompressLZ8(dest, dest2);
+                FreeToHeap(dest);
+            }
+            dest = dest2; // UB: if dest2 is NULL, dest is never freed
+        }
+    }
+    return dest;
+}
