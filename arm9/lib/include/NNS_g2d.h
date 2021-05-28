e8b7894e1c1496f4564a96c0a13ed5cac2000f6f
PikalaxALT <pikalaxalt@gmail.com>
2021-05-27 21:28:30 -0400

    Through FUN_02006AE4

--- arm9/lib/include/NNS_g2d.h
+++ arm9/lib/include/NNS_g2d.h
@@ -32,8 +32,112 @@ typedef struct NNSG2dScreenData
 }
 NNSG2dScreenData;
 
+typedef struct NNSG2dPaletteData
+{
 
-extern BOOL NNS_G2dGetUnpackedBGCharacterData( void* pNcgrFile, NNSG2dCharacterData** ppCharData );
-extern BOOL NNS_G2dGetUnpackedScreenData( void* pNscrFile, NNSG2dScreenData** ppScrData );
+    GXTexFmt            fmt;
+    BOOL                bExtendedPlt;
+
+    u32                 szByte;
+    void*               pRawData;          // offset addr of the content.
+
+}
+NNSG2dPaletteData;
+
+typedef enum NNS_G2D_VRAM_TYPE
+{
+    NNS_G2D_VRAM_TYPE_3DMAIN = 0,
+    NNS_G2D_VRAM_TYPE_2DMAIN = 1,
+    NNS_G2D_VRAM_TYPE_2DSUB  = 2,
+    NNS_G2D_VRAM_TYPE_MAX    = 3
+
+} NNS_G2D_VRAM_TYPE;
+
+typedef struct NNSG2dImageAttr
+{
+    GXTexSizeS                      sizeS;          // Image size (Invalid data is set during 1D mapping.)
+    GXTexSizeT                      sizeT;          // Image size (Invalid data is set during 1D mapping.)
+
+    GXTexFmt                        fmt;            // Image format
+    BOOL                            bExtendedPlt;   // Whether or not to use the extended palette
+
+    GXTexPlttColor0                 plttUse;        // How to use palette number 0
+    GXOBJVRamModeChar               mappingType;    // Mapping mode
+}
+NNSG2dImageAttr;
+
+typedef struct NNSG2dVRamLocation
+{
+    u32                 baseAddrOfVram[ NNS_G2D_VRAM_TYPE_MAX ];
+}
+NNSG2dVRamLocation;
+
+typedef struct NNSG2dImageProxy
+{
+    NNSG2dVRamLocation        vramLocation;
+    NNSG2dImageAttr           attr;
+}
+NNSG2dImageProxy;
+
+typedef struct NNSG2dPaletteCompressInfo
+{
+    u16               numPalette;
+    u16               pad16;
+    void*             pPlttIdxTbl;        // offset addr.
+
+}
+    NNSG2dPaletteCompressInfo;
+
+void NNS_G2dLoadImage1DMapping
+(
+    const NNSG2dCharacterData*  pSrcData,
+    u32                         baseAddr,
+    NNS_G2D_VRAM_TYPE           type,
+    NNSG2dImageProxy*           pImgProxy
+);
+
+void NNS_G2dLoadImage2DMapping
+(
+    const NNSG2dCharacterData*  pSrcData,
+    u32                         baseAddr,
+    NNS_G2D_VRAM_TYPE           type,
+    NNSG2dImageProxy*           pImgProxy
+);
+
+typedef struct NNSG2dImagePaletteProxy
+{
+
+    GXTexFmt                  fmt;          // palette format
+    // (Obtainable values are limited to GX_TEXFMT_PLTT16 and GX_TEXFMT_PLTT256)
+
+    BOOL                      bExtendedPlt; // Use expanded palette?
+
+    NNSG2dVRamLocation        vramLocation;
+}
+    NNSG2dImagePaletteProxy;
+
+void NNS_G2dLoadPalette
+(
+    const NNSG2dPaletteData*    pSrcData,
+    u32                         addr,
+    NNS_G2D_VRAM_TYPE           type,
+    NNSG2dImagePaletteProxy*    pPltProxy
+);
+
+void NNS_G2dLoadPaletteEx
+(
+    const NNSG2dPaletteData*            pSrcData,
+    const NNSG2dPaletteCompressInfo*    pCmpInfo,
+    u32                                 addr,
+    NNS_G2D_VRAM_TYPE                   type,
+    NNSG2dImagePaletteProxy*            pPltProxy
+);
+
+BOOL NNS_G2dGetUnpackedBGCharacterData( void* pNcgrFile, NNSG2dCharacterData** ppCharData );
+BOOL NNS_G2dGetUnpackedCharacterData( void* pNcgrFile, NNSG2dCharacterData** ppCharData );
+BOOL NNS_G2dGetUnpackedScreenData( void* pNscrFile, NNSG2dScreenData** ppScrData );
+BOOL NNS_G2dGetUnpackedPaletteData( void* pNclrFile, NNSG2dPaletteData** ppPltData );
+BOOL NNS_G2dGetUnpackedPaletteData( void* pNclrFile, NNSG2dPaletteData** ppPltData );
+BOOL NNS_G2dGetUnpackedPaletteCompressInfo( void* pNclrFile, NNSG2dPaletteCompressInfo** ppPltCmpInfo );
 
 #endif //NNSYS_NNS_G2D_H
--- arm9/src/gf_gfx_loader.c
+++ arm9/src/gf_gfx_loader.c
@@ -39,3 +39,138 @@ THUMB_FUNC void FUN_020068C8(NarcId narcId, s32 memberNo, struct UnkStruct_02016
         FreeToHeap(r7);
     }
 }
+
+void FUN_02006948(NarcId narcId, s32 memberNo, u32 r4, u32 r7, u32 r6, u32 r5, u32 heap_id);
+
+THUMB_FUNC void FUN_02006930(NarcId narcId, s32 memberNo, u32 r2, u32 r3, u32 sp10, u32 heap_id)
+{
+    FUN_02006948(narcId, memberNo, r2, 0, r3, sp10, heap_id);
+}
+
+THUMB_FUNC void FUN_02006948(NarcId narcId, s32 memberNo, u32 r4, u32 r7, u32 r6, u32 r5, u32 heap_id)
+{
+    static void (*const UNK_020ECBB0[])(void *, u32, u32) = {
+        GX_LoadBGPltt,
+        GX_LoadOBJPltt,
+        GX_LoadBGExtPltt,
+        GX_LoadOBJExtPltt,
+        GXS_LoadBGPltt,
+        GXS_LoadOBJPltt,
+        GXS_LoadBGExtPltt,
+        GXS_LoadOBJExtPltt
+    };
+    NNSG2dPaletteData * sp8;
+    void * sp4 = UncompressFromNarc(narcId, memberNo, FALSE, heap_id, TRUE);
+    if (sp4 != NULL)
+    {
+        if (NNS_G2dGetUnpackedPaletteData(sp4, &sp8))
+        {
+            sp8->pRawData = (void *)((u32)sp8->pRawData + r7);
+            if (r5 == 0)
+                r5 = sp8->szByte - r7;
+            DC_FlushRange(sp8->pRawData, r5);
+            switch (r4)
+            {
+            case 2:
+                GX_BeginLoadBGExtPltt();
+                UNK_020ECBB0[r4](sp8->pRawData, r6, r5);
+                GX_EndLoadBGExtPltt();
+                break;
+            case 6:
+                GXS_BeginLoadBGExtPltt();
+                UNK_020ECBB0[r4](sp8->pRawData, r6, r5);
+                GXS_EndLoadBGExtPltt();
+                break;
+            case 3:
+                GX_BeginLoadOBJExtPltt();
+                UNK_020ECBB0[r4](sp8->pRawData, r6, r5);
+                GX_EndLoadOBJExtPltt();
+                break;
+            case 7:
+                GXS_BeginLoadOBJExtPltt();
+                UNK_020ECBB0[r4](sp8->pRawData, r6, r5);
+                GXS_EndLoadOBJExtPltt();
+                break;
+            default:
+                UNK_020ECBB0[r4](sp8->pRawData, r6, r5);
+                break;
+            }
+        }
+        FreeToHeap(sp4);
+    }
+}
+
+THUMB_FUNC u32 FUN_02006A34(NarcId narcId, s32 memberId, u32 r5, u32 r6, u32 r4, BOOL isCompressed, u32 heap_id)
+{
+    static void (*const UNK_020ECBA0[])(void *, u32, u32) = {
+        GX_LoadOBJ,
+        GXS_LoadOBJ
+    };
+
+    NNSG2dCharacterData* sp4;
+    void * r7 = UncompressFromNarc(narcId, memberId, isCompressed, heap_id, TRUE);
+    if (r7 != NULL)
+    {
+        if (NNS_G2dGetUnpackedCharacterData(r7, &sp4))
+        {
+            if (r4 == 0)
+                r4 = sp4->szByte;
+            DC_FlushRange(sp4->pRawData, r4);
+            UNK_020ECBA0[r5](sp4->pRawData, r6, r4);
+        }
+        FreeToHeap(r7);
+    }
+    return r4;
+}
+
+THUMB_FUNC void FUN_02006A8C(NarcId narcId, s32 memberId, NNS_G2D_VRAM_TYPE r5, u32 r4, u32 heap_id, NNSG2dImagePaletteProxy * sp24)
+{
+    void * r6 = UncompressFromNarc(narcId, memberId, FALSE, heap_id, TRUE);
+    NNSG2dPaletteData* sp8;
+    NNSG2dPaletteCompressInfo* sp4;
+    if (r6 != NULL)
+    {
+        BOOL r7 = NNS_G2dGetUnpackedPaletteCompressInfo(r6, &sp4);
+        if (NNS_G2dGetUnpackedPaletteData(r6, &sp8))
+        {
+            if (r7)
+            {
+                NNS_G2dLoadPaletteEx(sp8, sp4, r4, r5, sp24);
+            }
+            else
+            {
+                NNS_G2dLoadPalette(sp8, r4, r5, sp24);
+            }
+        }
+        FreeToHeap(r6);
+    }
+}
+
+THUMB_FUNC u32 FUN_02006AE4(NarcId narcId, s32 memberId, BOOL isCompressed, u32 r5, u32 sp18, u32 sp1C, u32 sp20, u32 heap_id, NNSG2dImageProxy *sp28)
+{
+    static void (*const UNK_020ECBA8[])(const NNSG2dCharacterData *, u32, NNS_G2D_VRAM_TYPE, NNSG2dImageProxy *) = {
+        NNS_G2dLoadImage1DMapping,
+        NNS_G2dLoadImage2DMapping
+    };
+
+    void * r6 = UncompressFromNarc(narcId, memberId, isCompressed, heap_id, TRUE);
+    u32 r4 = 0;
+    NNSG2dCharacterData * sp4;
+    if (r6 != NULL)
+    {
+        if (NNS_G2dGetUnpackedCharacterData(r6, &sp4))
+        {
+            if (sp18 != 0)
+                sp4->szByte = sp18;
+            UNK_020ECBA8[r5](sp4, sp20, sp1C, sp28);
+            r4 = sp4->szByte;
+        }
+        FreeToHeap(r6);
+    }
+    return r4;
+}
+
+void (*const UNK_020ECB98[])(const NNSG2dCharacterData *, u32, NNS_G2D_VRAM_TYPE, NNSG2dImageProxy *) = {
+    NNS_G2dLoadImage1DMapping,
+    NNS_G2dLoadImage2DMapping
+};
