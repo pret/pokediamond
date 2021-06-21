64f35278f386af54f33172b64645220a4d51e502
Rémi Calixte <remicalixte.rmc@gmail.com>
2021-06-21 18:47:20 +0200

    add note about NONMATCHING

--- arm9/src/font.c
+++ arm9/src/font.c
@@ -63,6 +63,11 @@ THUMB_FUNC void FUN_02002CC0(s32 param0)
 #ifdef NONMATCHING
 THUMB_FUNC void FUN_02002CF8(s32 param0)
 {
+    /*
+    param0 is used as a index to a u32[]. The original assembly computes param0<<2 several times
+    in the function where the present attempt compute it once and keep it r6.
+    */
+
     GF_ASSERT(param0 < 4);
 
     if (UNK_02106FC8->unk84[param0] != NULL)
