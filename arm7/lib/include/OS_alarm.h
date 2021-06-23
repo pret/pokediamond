4195577deb16293f0e0cb93fe9a6da80cc0c97eb
Michael Panzlaff <michael.panzlaff@fau.de>
2021-06-23 13:20:51 +0200

    arm7: add OS_CancelAlarm declaration

--- arm7/lib/include/OS_alarm.h
+++ arm7/lib/include/OS_alarm.h
@@ -5,6 +5,7 @@
 #include "nitro/OS_alarm_shared.h"
 
 void OS_CreateAlarm(OSAlarm *alarm);
+void OS_CancelAlarm(OSAlarm *alarm);
 BOOL OS_IsAlarmAvailable(void);
 void OS_SetAlarm(OSAlarm *alarm, OSTick tick, OSAlarmHandler handler, void *arg);
 void OS_SetPeriodicAlarm(OSAlarm *alarm, OSTick start, OSTick period, OSAlarmHandler handler, void *arg);
