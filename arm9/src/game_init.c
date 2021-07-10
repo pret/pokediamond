795f3323f2649e89e3cfe89676bf0d0f182a6c5d
SNBeast <snburchett@gmail.com>
2021-07-10 03:07:05 +0000

    Fix the merge conflict right this time

--- arm9/src/game_init.c
+++ arm9/src/game_init.c
@@ -15,18 +15,6 @@
 
 #pragma thumb on
 
-typedef volatile struct
-{
-    u8 sys_and_irq_stack[0x3F80];
-    u8 svc_stack[0x40];
-    u8 reserved[0x38];
-    u32 intr_check;
-    void * intr_vector;
-}
-OS_DTCM;
-
-#define DTCM (*(OS_DTCM *)HW_DTCM)
-
 static struct {
     void * contents;
     u32 name_hash;
