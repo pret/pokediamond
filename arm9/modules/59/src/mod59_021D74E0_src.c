1d1a0987aafbe070be64fb3e67355dd472b5fb92
red031000 <rubenru09@aol.com>
2023-03-13 02:18:17 +0000

    MOD59_021D9F18

--- arm9/modules/59/src/mod59_021D74E0_src.c
+++ arm9/modules/59/src/mod59_021D74E0_src.c
@@ -33,8 +33,6 @@ extern BOOL MOD59_021D9898(struct UnkStruct_02006234 *param0, u32 *param1);
 extern BOOL MOD59_021D99F8(struct UnkStruct_02006234 *param0, u32 *param1);
 
 //todo move with the other rodata
-extern struct GraphicsBanks MOD59_021D9F18;
-
 extern const struct MOD59_ListStruct021D9E0C MOD59_021D9F40[5];
 extern const struct MOD59_ListStruct021D9E0C MOD59_021D9F68[5];
 
@@ -42,26 +40,15 @@ extern const struct MOD59_GraphicsPaletteMap021D9F90 MOD59_021D9F90;
 
 extern const u8 MOD59_021D9FE8[0x64];
 
-const struct WindowTemplate MOD59_021D9D90 =
+const struct WindowTemplate MOD59_021D9DB0 =
     {
         .bgId = GF_BG_LYR_MAIN_0,
-        .tilemapLeft = 8,
-        .tilemapTop = 0,
-        .width = 24,
-        .height = 24,
+        .tilemapLeft = 1,
+        .tilemapTop = 3,
+        .width = 16,
+        .height = 6,
         .paletteNum = 5,
-        .baseTile = 301,
-    };
-
-const struct WindowTemplate MOD59_021D9DB8 =
-    {
-        .bgId = GF_BG_LYR_MAIN_0,
-        .tilemapLeft = 2,
-        .tilemapTop = 19,
-        .width = 27,
-        .height = 4,
-        .paletteNum = 6,
-        .baseTile = 877,
+        .baseTile = 781,
     };
 
 const struct WindowTemplate MOD59_021D9DA0 =
@@ -75,6 +62,17 @@ const struct WindowTemplate MOD59_021D9DA0 =
         .baseTile = 737,
     };
 
+const struct WindowTemplate MOD59_021D9DB8 =
+    {
+        .bgId = GF_BG_LYR_MAIN_0,
+        .tilemapLeft = 2,
+        .tilemapTop = 19,
+        .width = 27,
+        .height = 4,
+        .paletteNum = 6,
+        .baseTile = 877,
+    };
+
 const struct WindowTemplate MOD59_021D9D98 =
     {
         .bgId = GF_BG_LYR_MAIN_0,
@@ -86,17 +84,6 @@ const struct WindowTemplate MOD59_021D9D98 =
         .baseTile = 301,
     };
 
-const struct WindowTemplate MOD59_021D9DB0 =
-    {
-        .bgId = GF_BG_LYR_MAIN_0,
-        .tilemapLeft = 1,
-        .tilemapTop = 3,
-        .width = 16,
-        .height = 6,
-        .paletteNum = 5,
-        .baseTile = 781,
-    };
-
 const struct WindowTemplate MOD59_021D9DA8 =
     {
         .bgId = GF_BG_LYR_MAIN_0,
@@ -106,6 +93,17 @@ const struct WindowTemplate MOD59_021D9DA8 =
         .height = 4,
         .paletteNum = 5,
         .baseTile = 853,
+    };
+
+const struct WindowTemplate MOD59_021D9D90 =
+    {
+        .bgId = GF_BG_LYR_MAIN_0,
+        .tilemapLeft = 8,
+        .tilemapTop = 0,
+        .width = 24,
+        .height = 24,
+        .paletteNum = 5,
+        .baseTile = 301,
     };
 
 const u32 MOD59_021D9DC0[3] = { NARC_intro_narc_0031_NCGR, NARC_intro_narc_0032_NCGR, 0xFFFF };
@@ -120,6 +118,11 @@ const struct MOD59_ListStruct021D9E0C MOD59_021D9E0C[2] =
             .msgNo = 35,
             .val = 2
         }
+    };
+
+const struct MOD59_CharStruct021D9DEC MOD59_021D9DFC =
+    {
+        .narcId = { NARC_intro_narc_0013_NCGR, NARC_intro_narc_0014_NCGR, NARC_intro_narc_0015_NCGR, NARC_intro_narc_0016_NCGR }
     };
 
 const struct GraphicsModes MOD59_021D9DCC =
@@ -143,11 +146,6 @@ const struct MOD59_CharStruct021D9DEC MOD59_021D9DEC =
         .narcId = { NARC_intro_narc_0008_NCGR, NARC_intro_narc_0009_NCGR, NARC_intro_narc_0010_NCGR, NARC_intro_narc_0011_NCGR }
     };
 
-const struct MOD59_CharStruct021D9DEC MOD59_021D9DFC =
-    {
-        .narcId = { NARC_intro_narc_0013_NCGR, NARC_intro_narc_0014_NCGR, NARC_intro_narc_0015_NCGR, NARC_intro_narc_0016_NCGR }
-    };
-
 const struct MOD59_UnkStruct021D9E30 MOD59_021D9E1C =
     {
         .scrnIds = { NARC_intro_narc_0003_NSCR, NARC_intro_narc_0004_NSCR, NARC_intro_narc_0005_NSCR, NARC_intro_narc_0006_NSCR, NARC_intro_narc_0007_NSCR }
@@ -159,6 +157,11 @@ const struct MOD59_UnkStruct021D9E30 MOD59_021D9E30 =
     };
 
 const u32 MOD59_021D9E44[5] = { 0, 6, 1, 12, 2060 };
+
+const struct MOD59_CharStruct021D9E70 MOD59_021D9E88 =
+    {
+        .charData = { NARC_intro_narc_0008_NCGR, NARC_intro_narc_0040_NCGR, NARC_intro_narc_0041_NCGR, NARC_intro_narc_0042_NCGR, NARC_intro_narc_0043_NCGR, 0xFF }
+    };
 
 const struct MOD59_ListStruct021D9E0C MOD59_021D9E58[3] =
     {
@@ -176,33 +179,9 @@ const struct MOD59_ListStruct021D9E0C MOD59_021D9E58[3] =
         }
     };
 
-
 const struct MOD59_CharStruct021D9E70 MOD59_021D9E70 =
     {
         .charData = { NARC_intro_narc_0013_NCGR, NARC_intro_narc_0044_NCGR, NARC_intro_narc_0045_NCGR, NARC_intro_narc_0046_NCGR, NARC_intro_narc_0047_NCGR, 0xFF }
-    };
-
-const struct MOD59_CharStruct021D9E70 MOD59_021D9E88 =
-    {
-        .charData = { NARC_intro_narc_0008_NCGR, NARC_intro_narc_0040_NCGR, NARC_intro_narc_0041_NCGR, NARC_intro_narc_0042_NCGR, NARC_intro_narc_0043_NCGR, 0xFF }
-    };
-
-const struct BgTemplate MOD59_021D9EBC =
-    {
-        .x = 0,
-        .y = 0,
-        .bufferSize = 2048,
-        .baseTile = 0,
-
-        .size = 1,
-        .colorMode = 0,
-        .screenBase = 0,
-        .charBase = 0,
-        .bgExtPltt = 0,
-        .priority = 0,
-        .areaOver = 0,
-        .unk17 = 0,
-        .mosaic = 0
     };
 
 const struct BgTemplate MOD59_021D9EA0 =
@@ -218,6 +197,24 @@ const struct BgTemplate MOD59_021D9EA0 =
         .charBase = 0,
         .bgExtPltt = 0,
         .priority = 1,
+        .areaOver = 0,
+        .unk17 = 0,
+        .mosaic = 0
+    };
+
+const struct BgTemplate MOD59_021D9EBC =
+    {
+        .x = 0,
+        .y = 0,
+        .bufferSize = 2048,
+        .baseTile = 0,
+
+        .size = 1,
+        .colorMode = 0,
+        .screenBase = 0,
+        .charBase = 0,
+        .bgExtPltt = 0,
+        .priority = 0,
         .areaOver = 0,
         .unk17 = 0,
         .mosaic = 0
@@ -246,6 +243,20 @@ const struct ListMenuTemplate MOD59_021D9EF8 =
         .fontId = 0,
         .cursorKind = 0,
         .unk_1C = 0
+    };
+
+const struct GraphicsBanks MOD59_021D9F18 =
+    {
+        .bg = 2,
+        .bgextpltt = 0,
+        .subbg = 4,
+        .subbgextpltt = 0,
+        .obj = 0,
+        .objextpltt = 0,
+        .subobj = 0,
+        .subobjextpltt = 0,
+        .tex = 0,
+        .texpltt = 0
     };
 
 extern void FUN_0200E1D0(u32 param0, u32 param1, u32 param2, u32 param3, u32 param4, u32 param5, u32 heap_id);
