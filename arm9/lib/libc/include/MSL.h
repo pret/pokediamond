13a9a5d6c487278e747dbf9d52d9a0ca5dfa7417
Akashi Akira <rubenru09@aol.com>
2023-05-08 21:29:56 +0100

    MSL_Common_rand

--- arm9/lib/libc/include/MSL.h
+++ arm9/lib/libc/include/MSL.h
@@ -4,5 +4,6 @@
 //include all msl files here
 
 #include "MSL_Common_arith.h"
+#include "MSL_Common_rand.h"
 
 #endif //MSL_H
---
+++ arm9/lib/libc/include/MSL_Common_rand.h
@@ -0,0 +1,9 @@
+#ifndef MSL_COMMON_RAND_H
+#define MSL_COMMON_RAND_H
+
+#include "nitro/types.h"
+
+s32 rand(void);
+void srand(u32 seed);
+
+#endif // MSL_COMMON_RAND_H
