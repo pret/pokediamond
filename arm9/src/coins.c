58c07cef724a9eb5e02540b00fa59ccda9593b0c
red031000 <rubenru09@aol.com>
2023-07-10 21:16:07 +0100

    cleanup coins.c

--- arm9/src/coins.c
+++ arm9/src/coins.c
@@ -1,42 +1,41 @@
 #include "global.h"
 #include "coins.h"
 
-void InitCoins(u16 * coins)
-{
+void InitCoins(u16 *coins) {
     *coins = 0;
 }
 
-void SetCoins(u16 * coins, u16 value)
-{
+void SetCoins(u16 *coins, u16 value) {
     GF_ASSERT(value <= MAX_COINS);
     *coins = value;
 }
 
-u16 CheckCoins(u16 * coins)
-{
+u16 CheckCoins(u16 *coins) {
     return *coins;
 }
 
-BOOL GiveCoins(u16 * coins, u16 amount)
-{
-    if (*coins >= MAX_COINS)
+BOOL GiveCoins(u16 * coins, u16 amount) {
+    if (*coins >= MAX_COINS) {
         return FALSE;
+    }
+
     *coins += amount;
-    if (*coins > MAX_COINS)
+    if (*coins > MAX_COINS) {
         *coins = MAX_COINS;
+    }
+
     return TRUE;
 }
 
-BOOL CanGiveCoins(u16 * coins, u16 amount)
-{
-
+BOOL CanGiveCoins(u16 *coins, u16 amount) {
     return (u32)(amount + *coins) <= MAX_COINS;
 }
 
-BOOL TakeCoins(u16 * coins, u16 amount)
-{
-    if (*coins < amount)
+BOOL TakeCoins(u16 *coins, u16 amount) {
+    if (*coins < amount) {
         return FALSE;
+    }
+
     *coins -= amount;
     return TRUE;
 }
--- include/coins.h
+++ include/coins.h
@@ -3,11 +3,11 @@
 
 #define MAX_COINS 50000
 
-void InitCoins(u16 * coins);
-void SetCoins(u16 * coins, u16 value);
-u16 CheckCoins(u16 * coins);
-BOOL GiveCoins(u16 * coins, u16 amount);
-BOOL CanGiveCoins(u16 * coins, u16 amount);
-BOOL TakeCoins(u16 * coins, u16 amount);
+void InitCoins(u16 *coins);
+void SetCoins(u16 *coins, u16 value);
+u16 CheckCoins(u16 *coins);
+BOOL GiveCoins(u16 *coins, u16 amount);
+BOOL CanGiveCoins(u16 *coins, u16 amount);
+BOOL TakeCoins(u16 *coins, u16 amount);
 
 #endif //POKEDIAMOND_COINS_H
