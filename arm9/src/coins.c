bfd7e311584e2e18a22eb8efac2c7f9c1cd32c59
Eduardo Quezada <eduardo602002@gmail.com>
2025-08-07 12:57:08 -0400

    Synced coins with pokeheartgold and pokeplatinum

--- arm9/src/coins.c
+++ arm9/src/coins.c
@@ -2,25 +2,26 @@
 
 #include "global.h"
 
-void InitCoins(u16 *coins) {
+void Coins_Init(u16 *coins) {
     *coins = 0;
 }
 
-void SetCoins(u16 *coins, u16 value) {
+void Coins_SetValue(u16 *coins, u32 value) {
     GF_ASSERT(value <= MAX_COINS);
     *coins = value;
 }
 
-u16 CheckCoins(u16 *coins) {
+u16 Coins_GetValue(u16 *coins) {
     return *coins;
 }
 
-BOOL GiveCoins(u16 *coins, u16 amount) {
+BOOL Coins_Add(u16 *coins, u16 amount) {
     if (*coins >= MAX_COINS) {
         return FALSE;
     }
 
     *coins += amount;
+
     if (*coins > MAX_COINS) {
         *coins = MAX_COINS;
     }
@@ -28,11 +29,11 @@ BOOL GiveCoins(u16 *coins, u16 amount) {
     return TRUE;
 }
 
-BOOL CanGiveCoins(u16 *coins, u16 amount) {
+BOOL Coins_CanAdd(u16 *coins, u16 amount) {
     return (u32)(amount + *coins) <= MAX_COINS;
 }
 
-BOOL TakeCoins(u16 *coins, u16 amount) {
+BOOL Coins_Subtract(u16 *coins, u16 amount) {
     if (*coins < amount) {
         return FALSE;
     }
